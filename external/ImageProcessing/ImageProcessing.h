1207e463b52eada6d611b1efa63aba33de4e0f1d
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2015-11-22 13:12:34 +0100

    Add external C++ project for Image Processing
    We need some of OpenCV's contrib modules, which aren't available in
    EmguCV. Might be prudent to have a separate C++ module for
    calculation-intensive algorithms anyway
    
    
    Former-commit-id: 8f11ef1711fc43d697ceddbd60da8184b4a85098

---
+++ external/ImageProcessing/ImageProcessing.h
@@ -0,0 +1,13 @@
+// ImageProcessing.h
+
+#pragma once
+
+using namespace System;
+
+namespace ImageProcessing {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
