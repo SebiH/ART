87fff779e9129543e2fce6358919ac45ef304261
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2015-11-22 22:05:50 +0100

    Add project for testing things without Unity
    Former-commit-id: cccf2f66ffa7c8fd191d10b75c5e3603ecc22b14

---
+++ external/ImageProcessingTest/Init.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace ImageProcessingTest
+{
+    class Init
+    {
+        static void Main(string[] args)
+        {
+            Console.Out.Write("Hello, testsuite!");
+        }
+    }
+}
