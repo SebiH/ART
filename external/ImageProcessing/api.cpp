a7516c196254154ad41eae49d005c60cbd3c32d4
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2015-11-23 17:34:14 +0100

    Cleanup, add 'unavoidable' error messages
    Error message will be drawn in image for now, so they're easier to see
    
    
    Former-commit-id: c2052022e80fe2cf45bd96e34e9e295a3b5280da

--- external/ImageProcessing/api.cpp
+++ external/ImageProcessing/api.cpp
@@ -17,120 +17,75 @@ static bool readCameraParameters(std::string filename, Mat &camMatrix, Mat &dist
 	return true;
 }
 
+static void drawError(Mat& img, const String error)
+{
+	// TODO:
+	// - line breaks?
+	// - multiple errors should appear underneath each other, not in same position 
+	putText(img, error, Point(0, 50), CV_FONT_HERSHEY_SIMPLEX, 0.5, Scalar(255, 0, 0), 1);
+}
+
 
 extern "C" DllExport unsigned char* DetectMarker(unsigned char *data, int width, int height)
 {
 	Mat image = Mat(height, width, CV_8UC3, data);
 
-	//VideoCapture cap(0); //capture the video from web cam
-	bool consoleActive = false;
-	std::ostringstream consoleCommand;
+	drawError(image, "Test test 123");
 
-	//if (!cap.isOpened())  // if not success, exit program
-	//{
-	//	std::cout << "Cannot open the web cam" << std::endl;
-	//	return NULL;
-	//}
+	aruco::Dictionary dictionary = aruco::getPredefinedDictionary(aruco::PREDEFINED_DICTIONARY_NAME(aruco::DICT_5X5_250));
 
-	bool useBlurring = false;
-
-	aruco::Dictionary dictionary =
-		aruco::getPredefinedDictionary(aruco::PREDEFINED_DICTIONARY_NAME(aruco::DICT_5X5_250));
-
-	//Mat marker;
-	//aruco::drawMarker(dictionary, 1, 640, marker, 1);
-	//imshow("x", marker);
+	// parameters from printed board
+	int markersX = 5;
+	int markersY = 3;
+	int markerLength = 300;
+	int markerSeparation = 75;
 
 	auto board = aruco::GridBoard::create(5, 3, 300, 75, dictionary);
-	//float axisLength = 0.5f * ((float)min(markersX, markersY) * (markerLength + markerSeparation) + markerSeparation);
-	float axisLength = 0.5f * ((float)min(5, 3) * (300 + 75) + 75);
+	float axisLength = 0.5f * ((float)min(markersX, markersY) * (markerLength + markerSeparation) + markerSeparation);
 
 	bool showRejected = false;
 	bool estimatePose = true;
-	float markerLength = 1.0f;
+
 	aruco::DetectorParameters detectorParams; // TODO?
 	detectorParams.doCornerRefinement = true; // do corner refinement in markers
 
 	Mat camMatrix, distCoeffs;
 	auto cameraParamFilename = "Assets/CV/webcam.yml";
 	bool readOk = readCameraParameters(cameraParamFilename, camMatrix, distCoeffs);
-	if (!readOk) {
-		std::cerr << "Invalid camera file " << cameraParamFilename << std::endl;
-
-		for (int i = 0; i < width * height; i++)
-		{
-			if (i % 3 == 2)
-				data[i] = 255;
-			else
-				data[i] = 0;
-		}
-
-		return NULL;
+	if (!readOk)
+	{
+		drawError(image, "Could not find image calibration file!");
 	}
-	estimatePose = true;
 
 
+	std::vector< int > ids;
+	std::vector< std::vector< Point2f > > corners, rejected;
+	Vec3d rvec, tvec;
 
-	double totalTime = 0;
-	int totalIterations = 0;
-	int waitTime = 30;
+	// detect markers and estimate pose
+	int markersOfBoardDetected = 0;
+	aruco::detectMarkers(image, dictionary, corners, ids, detectorParams, rejected);
 
-		//Mat image, imageCopy;
+	if (estimatePose && ids.size() > 0)
+	{
+		markersOfBoardDetected = aruco::estimatePoseBoard(corners, ids, board, camMatrix, distCoeffs, rvec, tvec);
+	}
 
-		// read a new frame from video
-		//bool bSuccess = cap.read(image);
+	// draw results
+	if (ids.size() > 0)
+	{
+		aruco::drawDetectedMarkers(image, corners, ids);
+	}
 
-		// break on error
-		//if (!bSuccess)
-		//{
-		//	std::cout << "Cannot read a frame from video stream" << std::endl;
-		//	break;
-		//}
+	if (showRejected && rejected.size() > 0)
+	{
+		aruco::drawDetectedMarkers(image, rejected, noArray(), Scalar(100, 0, 255));
+	}
 
-		if (useBlurring)
-		{
-			GaussianBlur(image, image, cv::Size(3, 3), 5, 5);
-			circle(image, cv::Point(50, 50), 20, cv::Scalar(255, 0, 0));
-		}
-		else
-		{
-			circle(image, cv::Point(50, 50), 20, cv::Scalar(255, 0, 0));
-		}
-
-		double tick = (double)getTickCount();
-
-		std::vector< int > ids;
-		std::vector< std::vector< Point2f > > corners, rejected;
-		Vec3d rvec, tvec;
-
-		// detect markers and estimate pose
-		int markersOfBoardDetected = 0;
-		aruco::detectMarkers(image, dictionary, corners, ids, detectorParams, rejected);
-
-		if (estimatePose && ids.size() > 0)
-		{
-			markersOfBoardDetected = aruco::estimatePoseBoard(corners, ids, board, camMatrix, distCoeffs, rvec, tvec);
-		}
-
-		double currentTime = ((double)getTickCount() - tick) / getTickFrequency();
-		totalTime += currentTime;
-		totalIterations++;
-		if (totalIterations % 30 == 0)
-		{
-			std::cout << "Detection Time = " << currentTime * 1000 << " ms "
-				<< "(Mean = " << 1000 * totalTime / double(totalIterations) << " ms)" << std::endl;
-		}
-
-		// draw results
-		if (ids.size() > 0) {
-			aruco::drawDetectedMarkers(image, corners, ids);
-		}
-
-		if (showRejected && rejected.size() > 0)
-			aruco::drawDetectedMarkers(image, rejected, noArray(), Scalar(100, 0, 255));
-
-		if (markersOfBoardDetected > 0)
-			aruco::drawAxis(image, camMatrix, distCoeffs, rvec, tvec, axisLength);
+	if (markersOfBoardDetected > 0)
+	{
+		aruco::drawAxis(image, camMatrix, distCoeffs, rvec, tvec, axisLength);
+	}
 
 	// TODO return ... something?
 	return NULL;
