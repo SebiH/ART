703e4b8f5f39d8d767d17e7d6e5208c2f9f41c5a
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2015-12-06 16:05:06 +0100

    Allow console to be disabled via ` key
    Former-commit-id: 2407a3f7f0d84d26b193ed1fa547480eda6c530a

--- Assets/Scripts/GameConsole.cs
+++ Assets/Scripts/GameConsole.cs
@@ -8,6 +8,8 @@ public class GameConsole : MonoBehaviour {
     public InputField commandComponent;
     private UConsole console;
 
+    public bool isActive;
+
     private UConsoleCommand helpCmd;
 	void Start ()
     {
@@ -16,37 +18,46 @@ public class GameConsole : MonoBehaviour {
 
     void Update()
     {
-        foreach (char c in Input.inputString)
+        // enable or disable console
+        if (Input.GetKeyDown(KeyCode.BackQuote))
         {
-            console.CurrentInput += c;
+            isActive = !isActive;
         }
 
-        if (Input.GetKeyDown(KeyCode.Return))
+        if (isActive)
         {
-            console.ExecuteCommand();
-        }
-        
-        if (Input.GetKeyDown(KeyCode.Backspace))
-        {
-            if (console.CurrentInput.Length > 2)
+            foreach (char c in Input.inputString)
             {
-                console.CurrentInput = console.CurrentInput.Substring(0, console.CurrentInput.Length - 2);
+                console.CurrentInput += c;
             }
-            else
+
+            if (Input.GetKeyDown(KeyCode.Return))
             {
-                console.CurrentInput = "";
+                console.ExecuteCommand();
             }
+
+            if (Input.GetKeyDown(KeyCode.Backspace))
+            {
+                if (console.CurrentInput.Length > 2)
+                {
+                    console.CurrentInput = console.CurrentInput.Substring(0, console.CurrentInput.Length - 2);
+                }
+                else
+                {
+                    console.CurrentInput = "";
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var logline in console.Log)
+            {
+                sb.AppendLine(logline);
+            }
+
+            logComponent.text = sb.ToString();
+
+            commandComponent.text = console.CurrentInput;
         }
-
-        StringBuilder sb = new StringBuilder();
-        foreach (var logline in console.Log)
-        {
-            sb.AppendLine(logline);
-        }
-
-        logComponent.text = sb.ToString();
-
-        commandComponent.text = console.CurrentInput;
     }
 	
 }
