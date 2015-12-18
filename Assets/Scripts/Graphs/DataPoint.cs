09f03b3b36b2e09db59a39c47504e47b9b05f450
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2015-12-18 17:24:11 +0100

    Add SetHeight and SetPosition to DataPoints
    Former-commit-id: 3291edcd383409f3dac4a76f90272abfdcfd8170

--- Assets/Scripts/Graphs/DataPoint.cs
+++ Assets/Scripts/Graphs/DataPoint.cs
@@ -1,15 +1,30 @@
 using UnityEngine;
-using System.Collections;
 
-public class DataPoint : MonoBehaviour {
+public class DataPoint : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 	
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 	
 	}
+
+
+    public void SetHeight(float height)
+    {
+        // TODO: animation?
+        var currPos = transform.localPosition;
+        transform.localPosition = new Vector3(currPos.x, height / 2, currPos.z);
+        transform.localScale = new Vector3(1, height, 1);
+    }
+
+    public void SetPosition(float x, float y)
+    {
+        var currPos = transform.localPosition;
+        transform.localPosition = new Vector3(x, currPos.y, y);
+    }
 }
