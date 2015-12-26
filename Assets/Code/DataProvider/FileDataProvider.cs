c64b5564cc59a1930ca792673eeb09ae6c27f523
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2015-12-26 18:42:00 +0100

    Add basic DataProvider for CSV files
    Former-commit-id: b124b8fc8170a05e79a5c493ac571e83365b769b

---
+++ Assets/Code/DataProvider/FileDataProvider.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using System.IO;
+
+namespace Assets.Code.DataProvider
+{
+    class FileDataProvider : IDataProvider
+    {
+        private float[,] data;
+
+        public void ReadData(string path)
+        {
+            List<List<float>> dataList = new List<List<float>>();
+
+            // TODO: file not found?
+            using (var reader = new StreamReader(File.OpenRead(path)))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    var row = new List<float>();
+
+                    foreach (var val in values)
+                    {
+                        row.Add(float.Parse(val));
+                    }
+
+                    dataList.Add(row);
+                }
+            }
+
+            // assuming all rows have equal length
+            data = new float[dataList.Count + 1, dataList[0].Count + 1];
+
+            int i = 0;
+
+            foreach (var row in dataList)
+            {
+                int j = 0;
+
+                foreach (var entry in row)
+                {
+                    data[i, j] = entry;
+                    j++;
+                }
+
+                i++;
+            }
+        }
+
+
+        public float[,] GetData()
+        {
+            return data;
+        }
+    }
+}
