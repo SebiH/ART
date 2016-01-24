9f41fb34f62e9dc17cb59a11866de0a99cc71dd1
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2016-01-24 12:51:38 +0100

    Make trigger finger a parameter
    Former-commit-id: 9437785149a05a63a52f7706bc2caac73d48b029

--- Assets/Scripts/TriggerGestures/PinchTrigger.cs
+++ Assets/Scripts/TriggerGestures/PinchTrigger.cs
@@ -3,18 +3,29 @@ using System.Collections;
 
 public class PinchTrigger : MonoBehaviour {
 
+    public string TriggerFingerTag = "index";
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "index")
+        if (other.tag == TriggerFingerTag)
         {
-            print("On Trigger entered");
+            print("On Trigger enter");
+        }
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == TriggerFingerTag)
+        {
+            print("On Trigger stay");
         }
     }
 
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag == "index")
+        if (other.tag == TriggerFingerTag)
         {
             print("On Trigger exit");
         }
