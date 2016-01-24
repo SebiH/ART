02b5ab3509d7da0a62a3a01c8e19d8a87a5db245
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2016-01-24 21:42:09 +0100

    Use timeout to make gesture tracking more stable
    Handtracking is prone to errors, and losing the gesture can happen
    within a single frame. This may/will cause unwanted effects; it's
    probably better to use a timeout to smooth out the lost frames.
    
    
    Former-commit-id: 40023bb4939688af9c0e91fd81eb996d61c06190

--- Assets/Scripts/TriggerGestures/PinchTrigger.cs
+++ Assets/Scripts/TriggerGestures/PinchTrigger.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 public class PinchTrigger : BaseTrigger {
 
     public string TriggerFingerTag = "index";
+    public float GestureLostTimeout = 0.5f;
+
+    private bool IsGestureDetected;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == TriggerFingerTag)
         {
-            FireGestureDetected(GetCollisionPosition(other));
             StopCoroutine("TimeoutGestureStop");
+
+            if (!IsGestureDetected)
+            {
+                FireGestureDetected(GetCollisionPosition(other));
+                IsGestureDetected = true;
+            }
         }
     }
 
@@ -28,8 +36,23 @@ public class PinchTrigger : BaseTrigger {
     {
         if (other.tag == TriggerFingerTag)
         {
-            FireGestureStop(GetCollisionPosition(other));
+            StartCoroutine("TimeoutGestureStop", GetCollisionPosition(other));
         }
+    }
+
+    private IEnumerator TimeoutGestureStop(Vector3 pos)
+    {
+        float totalTime = 0f;
+
+        while (totalTime < GestureLostTimeout)
+        {
+            totalTime += Time.deltaTime;
+            yield return null;
+        }
+
+        // threshold reached
+        IsGestureDetected = false;
+        FireGestureStop(pos);
     }
 
 
