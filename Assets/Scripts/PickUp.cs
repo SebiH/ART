d80100d2b18dc133230a906cf34f532aed234f24
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2016-01-27 21:34:28 +0100

    Improve picking up objects .. slightly
    Former-commit-id: 3ccec9c7bb772ddb6edc44ef959cb68ec5b7e716

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class PickUp : MonoBehaviour
 {
-    private FixedJoint joint;
+    private SpringJoint joint;
 
     public void StartPickup()
     {
-        var finger = GameObject.FindGameObjectsWithTag("index")[0];
-        joint = finger.gameObject.AddComponent<FixedJoint>();
+        var finger = GameObject.FindGameObjectsWithTag("thumb")[0];
+        joint = finger.gameObject.AddComponent<SpringJoint>();
         joint.connectedBody = GetComponent<Rigidbody>();
+        joint.spring = 60f;
+        joint.damper = 0.2f;
+        joint.maxDistance = 0f;
+        joint.minDistance = 0f;
     }
 
     public void EndPickup()
