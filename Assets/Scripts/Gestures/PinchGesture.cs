40f1cdff2a5d9ac70c1577ed1c6c6d3e5e3d59d2
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2016-02-27 21:26:41 +0100

    Add experimental pinch gesture
    Former-commit-id: 22b9f8f9c4a7bd677386b6d855ec7e83370c3ea8

---
+++ Assets/Scripts/Gestures/PinchGesture.cs
@@ -0,0 +1,52 @@
+using System;
+using GestureControl;
+using UnityEngine;
+
+public class PinchGesture : GestureBase
+{
+    public bool TriggerOnLeftHand = true;
+    public bool TriggerOnRightHand = true;
+
+    private bool IsTriggeredLeft = false;
+    private bool IsTriggeredRight = false;
+
+    public override string GetName()
+    {
+        return "PinchGesture";
+    }
+
+    public override bool CheckConditions()
+    {
+        bool hasTriggered = false;
+
+        if (TriggerOnLeftHand)
+        {
+            var leftThumb = GestureSystem.GetLimb(InteractionLimb.LeftThumbTip);
+            var leftIndex = GestureSystem.GetLimb(InteractionLimb.LeftIndexTip);
+        }
+
+
+        if (TriggerOnRightHand)
+        {
+            var rightThumb = GestureSystem.GetLimb(InteractionLimb.RightThumbTip);
+            var rightIndex = GestureSystem.GetLimb(InteractionLimb.RightIndexTip);
+
+            if (GestureUtil.CollidesWith(rightThumb, rightIndex))
+            {
+                if (!IsTriggeredRight)
+                {
+                    Debug.Log("Triggered on right hand");
+                    hasTriggered = true;
+                    IsTriggeredRight = true;
+                }
+            }
+            else if (!IsTriggeredRight)
+            {
+                Debug.Log("No longer triggered on right hand");
+                IsTriggeredRight = true;
+            }
+        }
+
+        return hasTriggered;
+    }
+}
