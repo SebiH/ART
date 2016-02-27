8bb772292810b7ec3f01e390cc41eb25f2c46422
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2016-02-27 19:21:37 +0100

    Move InteractionLimb enum to own file
    Former-commit-id: 496d28c07978c47863a4435b50cda378dc94ac2b

--- Assets/Scripts/Gesture/GestureSystem.cs
+++ Assets/Scripts/Gesture/GestureSystem.cs
@@ -7,25 +7,6 @@ namespace Assets.Scripts.Gesture
 {
     public class GestureManager : MonoBehaviour
     {
-        public enum InteractionLimb
-        {
-            Left_Thumb_Tip,
-            Left_Index_Tip,
-            Left_Middle_Tip,
-            Left_Ring_Tip,
-            Left_Little_Tip,
-
-            Left_Palm,
-
-            Right_Thumb_Tip,
-            Right_Index_Tip,
-            Right_Middle_Tip,
-            Right_Ring_Tip,
-            Right_Little_Tip,
-
-            Right_Palm
-        }
-
         private static GameObject[] RegisteredLimbs = new GameObject[Enum.GetNames(typeof(InteractionLimb)).Length];
 
         public static void RegisterLimb(InteractionLimb type, GameObject limb)
