9b182613eb15e8bb3f7ef90d93aadd5e19246e9f
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2016-02-27 19:49:09 +0100

    Add events
    Former-commit-id: 24b830bceaef19904a8bdd985e9965edacec44fb

--- Assets/Scripts/Gesture/BaseGesture.cs
+++ Assets/Scripts/Gesture/BaseGesture.cs
@@ -10,6 +10,42 @@ namespace Assets.Scripts.Gesture
 
 
 
+        public delegate void GestureEventHandler(GestureBase gesture, GestureEvent e);
+
+        public event GestureEventHandler GestureStart;
+        public event GestureEventHandler GestureHold;
+        public event GestureEventHandler GestureEnd;
+
+        protected void OnGestureStart(GestureEvent e)
+        {
+            if (GestureStart != null)
+            {
+                GestureStart(this, e);
+            }
+        }
+
+
+        protected void OnGestureHold(GestureEvent e)
+        {
+            if (GestureHold != null)
+            {
+                GestureHold(this, e);
+            }
+        }
+
+
+        protected void OnGestureEnd(GestureEvent e)
+        {
+            if (GestureEnd != null)
+            {
+                GestureEnd(this, e);
+            }
+        }
+
+
+
+
+
         protected void Start()
         {
             GestureSystem.RegisterGesture(this);
