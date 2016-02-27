07ab05d6cc3425250fa320152ec6cb7c5da34f13
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2016-02-27 19:43:17 +0100

    Register BaseGesture on Start
    Former-commit-id: 9468647d65dbea609836fb2cc94a3578e42a807c

--- Assets/Scripts/Gesture/BaseGesture.cs
+++ Assets/Scripts/Gesture/BaseGesture.cs
@@ -1,14 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using UnityEngine;
 
 namespace Assets.Scripts.Gesture
 {
-    public abstract class GestureBase
+    public abstract class GestureBase : MonoBehaviour
     {
         public abstract string GetName();
 
         public abstract bool CheckConditions();
+
+
+
+        protected void Start()
+        {
+            GestureSystem.RegisterGesture(this);
+        }
     }
 }
