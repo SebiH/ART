0c13e6bf06949707ce8fa4315830bde130a785b9
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2016-03-01 21:12:06 +0100

    Increase slider responsiveness
    By batching the camera updates in intervals, rather than executing them
    directly
    
    
    Former-commit-id: c6b4505dc6d46015f6ddb136eab09a7cee9ff3fa

--- Assets/Scripts/TESTOvrGainSlider.cs
+++ Assets/Scripts/TESTOvrGainSlider.cs
@@ -5,20 +5,51 @@ using LMWidgets;
 public class TESTOvrGainSlider : SliderDemo
 {
     public GameObject OvrCamera;
-    private Ovrvision OvrScript;
+    private Ovrvision _ovrScript;
+    private int _expectedConfidence;
+    private bool _hasSliderChanged;
 
     protected override void Start()
     {
         base.Start();
         ChangeHandler += TESTOvrGainSlider_ChangeHandler;
-        OvrScript = OvrCamera.GetComponent<Ovrvision>();
+        _ovrScript = OvrCamera.GetComponent<Ovrvision>();
     }
 
     private void TESTOvrGainSlider_ChangeHandler(object sender, LMWidgets.EventArg<float> arg)
     {
         var fraction = GetSliderFraction();
-        OvrScript.conf_gain = (int)(Mathf.Floor(fraction * 47));
-        OvrScript.UpdateOvrvisionSetting();
+        _expectedConfidence = (int)(Mathf.Floor(fraction * 47));
+        _hasSliderChanged = true;
+    }
+
+
+    private float _timeSinceLastUpdate;
+    private float _minTimeBetweenUpdates = 0.5f;
+
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+
+        // only change the actual value if the change came from this script
+        if (!_hasSliderChanged)
+        {
+            // if the change didn't come from this script, make sure we're updating
+            // the slider to the current value
+            _expectedConfidence = _ovrScript.conf_gain;
+            SetPositionFromFraction(_expectedConfidence / 47f);
+            return;
+        }
+
+        _timeSinceLastUpdate += Time.deltaTime;
+
+        if (_timeSinceLastUpdate > _minTimeBetweenUpdates)
+        {
+            _ovrScript.conf_gain = _expectedConfidence;
+            _ovrScript.UpdateOvrvisionSetting();
+            _hasSliderChanged = false;
+            _timeSinceLastUpdate = 0;
+        }
     }
 
 }
