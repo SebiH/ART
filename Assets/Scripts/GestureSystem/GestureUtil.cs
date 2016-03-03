df1e7a9590a8739d4e8a5d4b609e9039e5539e21
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2016-03-03 14:30:05 +0100

    Implement missing IsInProximity method
    Former-commit-id: 5ce89063059f8c5ae23d256f14b4ea4e70258ae7

--- Assets/Scripts/GestureSystem/GestureUtil.cs
+++ Assets/Scripts/GestureSystem/GestureUtil.cs
@@ -20,9 +20,43 @@ namespace GestureControl
             return c1.bounds.Intersects(c2.bounds);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="distanceThreshold">Distance threshold in centimeters</param>
+        /// <param name="objects"></param>
+        /// <returns></returns>
         public static bool IsInProximity(double distanceThreshold, IEnumerable<GameObject> objects)
         {
-            throw new NotImplementedException();
+            // build the average position to apply distanceThreshold to
+            Vector3 averagePosition = Vector3.zero;
+            int objectsCount = 0;
+
+            foreach (var gameObject in objects)
+            {
+                averagePosition += gameObject.transform.position;
+                objectsCount++;
+            }
+
+            if (objectsCount == 0)
+            {
+                Debug.LogError("Tried to call IsInProximity without objects!");
+                return false;
+            }
+
+            averagePosition = averagePosition / objectsCount;
+
+
+            // check if all objects are within the threshold to the average position of all objects
+            foreach (var gameObject in objects)
+            {
+                if (Vector3.Distance(gameObject.transform.position, averagePosition) > distanceThreshold)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
