796d0e618a2e1771675f81c46a8ecf3da471f3f8
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2016-03-03 14:29:52 +0100

    Add two handed pinch (and drag) gesture. Closes #35
    Former-commit-id: 9ea5ea68ece6c8644a8548ffb851b3e66e025735

---
+++ Assets/Scripts/Gestures/DoublePinchDragGesture.cs
@@ -0,0 +1,56 @@
+using GestureControl;
+using UnityEngine;
+
+namespace Assets.Scripts.Gestures
+{
+    public class DoublePinchDragGesture : GestureBase
+    {
+        public float ActivationDistance = 0.03f;
+
+        private bool IsGestureActive;
+
+        public override string GetName()
+        {
+            return "Double Pinch and Drag Gesture";
+        }
+
+
+        public override bool CheckConditions()
+        {
+            var leftThumb = GestureSystem.GetLimb(InteractionLimb.LeftThumbTip);
+            var leftIndex = GestureSystem.GetLimb(InteractionLimb.LeftIndexTip);
+
+            var rightThumb = GestureSystem.GetLimb(InteractionLimb.RightThumbTip);
+            var rightIndex = GestureSystem.GetLimb(InteractionLimb.RightIndexTip);
+
+            if (IsGestureActive)
+            {
+                // if the gesture is already active, we only need to check if the user is still pinching their fingers together
+                if (GestureUtil.CollidesWith(leftThumb, leftIndex) && GestureUtil.CollidesWith(rightThumb, rightIndex))
+                {
+                    // gesture still active!
+                    OnGestureHold();
+                }
+                else
+                {
+                    // one of the hands stopped pinching - stop gesture
+                    IsGestureActive = false;
+                    OnGestureEnd();
+                }
+
+            }
+            else
+            {
+                // gesture not active, so we'll check if the gesture is initiated
+                if (GestureUtil.IsInProximity(ActivationDistance, new[] { leftThumb, leftIndex, rightThumb, rightIndex }))
+                {
+                    IsGestureActive = true;
+                    OnGestureStart();
+                }
+            }
+
+
+            return IsGestureActive;
+        }
+    }
+}
