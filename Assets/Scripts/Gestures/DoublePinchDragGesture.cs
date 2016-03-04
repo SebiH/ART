fb3cfc8b6aa3f74bae4e4c3b3fbc9271174c5567
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2016-03-04 15:25:45 +0100

    Save locations where doublepinchgesture occured
    (for use within scripts)
    
    
    Former-commit-id: 0158ae4aee0a3062de49ce2dc7170d9263389664

--- Assets/Scripts/Gestures/DoublePinchDragGesture.cs
+++ Assets/Scripts/Gestures/DoublePinchDragGesture.cs
@@ -1,4 +1,5 @@
 using Assets.Scripts.GestureControl;
+using System;
 using UnityEngine;
 
 namespace Assets.Scripts.Gestures
@@ -8,6 +9,43 @@ namespace Assets.Scripts.Gestures
         public float ActivationDistance = 0.03f;
 
         private bool IsGestureActive;
+
+
+
+        // Position of the gesture occurence of the left hand
+        private Vector3 GesturePosLeft;
+
+        // Position of the gesture occurence of the right hand
+        private Vector3 GesturePosRight;
+        
+        // Center position of the gesture
+        private Vector3 GesturePosCenter;
+
+        public Vector3 GetGesturePosition(Hand hand)
+        {
+            Vector3 pos = Vector3.zero;
+
+            switch (hand)
+            {
+                case Hand.Left:
+                    pos = GesturePosLeft;
+                    break;
+
+                case Hand.Right:
+                    pos = GesturePosRight;
+                    break;
+
+                case Hand.Both:
+                    pos = GesturePosCenter;
+                    break;
+
+                default:
+                    throw new NotSupportedException("Hand " + hand.ToString() + " is not supported");
+            }
+            
+            return pos;
+        }
+
 
         public override string GetName()
         {
@@ -28,7 +66,11 @@ namespace Assets.Scripts.Gestures
                 // if the gesture is already active, we only need to check if the user is still pinching their fingers together
                 if (GestureUtil.CollidesWith(leftThumb, leftIndex) && GestureUtil.CollidesWith(rightThumb, rightIndex))
                 {
-                    // gesture still active!
+                    // gesture still active
+                    GesturePosLeft = GestureUtil.GetAveragePosition(new[] { leftThumb, leftIndex });
+                    GesturePosRight = GestureUtil.GetAveragePosition(new[] { rightThumb, rightIndex });
+                    GesturePosCenter = GestureUtil.GetAveragePosition(new[] { leftThumb, leftIndex, rightThumb, rightIndex });
+
                     RaiseGestureActiveEvent();
                 }
                 else
@@ -45,6 +87,11 @@ namespace Assets.Scripts.Gestures
                 if (GestureUtil.IsInProximity(ActivationDistance, new[] { leftThumb, leftIndex, rightThumb, rightIndex }))
                 {
                     IsGestureActive = true;
+
+                    GesturePosLeft = GestureUtil.GetAveragePosition(new[] { leftThumb, leftIndex });
+                    GesturePosRight = GestureUtil.GetAveragePosition(new[] { rightThumb, rightIndex });
+                    GesturePosCenter = GestureUtil.GetAveragePosition(new[] { leftThumb, leftIndex, rightThumb, rightIndex });
+
                     RaiseGestureStartEvent();
                 }
             }
