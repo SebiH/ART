c69af55fae43d98613c592215c85fd6035549ad8
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2016-03-05 17:03:23 +0100

    Adjust scale of created object
    Former-commit-id: dc707bbf51712c6181778c823f187dc53acba0ea

--- Assets/Scripts/CreateObject.cs
+++ Assets/Scripts/CreateObject.cs
@@ -1,11 +1,13 @@
-using UnityEngine;
-using System.Collections;
 using Assets.Scripts.GestureControl;
 using Assets.Scripts.Gestures;
+using UnityEngine;
 
 public class CreateObject : MonoBehaviour
 {
     public GameObject Template;
+
+    public float MinScale = 0.05f;
+    public float MaxScale = 0.25f;
 
     private GameObject CreatedInstance;
 
@@ -17,14 +19,16 @@ public class CreateObject : MonoBehaviour
         {
             CreatedInstance = Instantiate(Template);
             CreatedInstance.transform.position = gesture.GetGesturePosition(Hand.Both);
-            CreatedInstance.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+
+            var scale = GetScale(gesture);
+            CreatedInstance.transform.localScale = new Vector3(scale, scale, scale);
+
             // turn off rigidbody during creation
             var rigidbody = CreatedInstance.GetComponent<Rigidbody>();
             rigidbody.detectCollisions = false;
-            rigidbody.mass *= 0.01f; // TODO: scale this proportional to the object's size
+            rigidbody.mass = scale;
         }
     }
-
 
     public void ModifyCreation(GestureEventArgs e)
     {
@@ -33,6 +37,12 @@ public class CreateObject : MonoBehaviour
         if (gesture != null)
         {
             CreatedInstance.transform.position = gesture.GetGesturePosition(Hand.Both);
+
+            var scale = GetScale(gesture);
+            CreatedInstance.transform.localScale = new Vector3(scale, scale, scale);
+
+            var rigidbody = CreatedInstance.GetComponent<Rigidbody>();
+            rigidbody.mass = scale;
         }
     }
 
@@ -49,6 +59,20 @@ public class CreateObject : MonoBehaviour
 
             CreatedInstance = null;
         }
+    }
+
+
+    private float GetScale(IPositionGesture gesture)
+    {
+        var gesturePosLeft = gesture.GetGesturePosition(Hand.Left);
+        var gesturePosRight = gesture.GetGesturePosition(Hand.Right);
+
+        var scale = (gesturePosLeft - gesturePosRight).magnitude;
+
+        // reduce scale a bit to avoid operlapping with fingers
+        scale -= 0.025f;
+
+        return Mathf.Clamp(scale, MinScale, MaxScale);
     }
 
 }
