3ab0df4749393166a8a1b7bb1d7c282a623d3936
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2016-03-26 00:53:11 +0100

    Implement methods to intercept OVRvision calls
    Former-commit-id: 0fbd1e8aeea85e2f99fde1208e2e1a7f664137d2

--- external/ImageProcessing/api.cpp
+++ external/ImageProcessing/api.cpp
@@ -7,9 +7,91 @@
 
 using namespace cv;
 
-extern "C" DllExport void Init()
+OVR::OvrvisionPro ovrCamera;
+int camWidth, camHeight;
+
+extern "C" DllExport void Start(int cameraMode = -1)
 {
-	// TODO.
+	OVR::Camprop camProp = (cameraMode == -1) ? OVR::OV_CAMVR_FULL : (OVR::Camprop)cameraMode;
+	ovrCamera = OVR::OvrvisionPro();
+
+	// TODO: error on failure?
+	auto openSuccess = ovrCamera.Open(0, camProp);
+
+
+	// default settings
+	ovrCamera.SetCameraExposure(12960);
+	ovrCamera.SetCameraGain(47);
+	ovrCamera.SetCameraSyncMode(false);
+
+	// store attributes for later
+	camWidth = ovrCamera.GetCamWidth();
+	camHeight = ovrCamera.GetCamHeight();
 }
 
 
+extern "C" DllExport void Stop()
+{
+	ovrCamera.Close();
+}
+
+
+extern "C" DllExport float GetAttribute(std::string *attribute)
+{
+	if (*attribute == "width")
+	{
+		return (float)ovrCamera.GetCamWidth();
+	}
+	else if (*attribute == "height")
+	{
+		return (float)ovrCamera.GetCamHeight();
+	}
+	else if (*attribute == "exposure")
+	{
+		return (float)ovrCamera.GetCameraExposure();
+	}
+	else if (*attribute == "gain")
+	{
+		return (float)ovrCamera.GetCameraGain();
+	}
+	else
+	{
+		// TODO: throw warning about unknown attribute
+		return .0f;
+	}
+}
+
+
+extern "C" DllExport void SetAttribute(std::string *attribute, float value)
+{
+	// TODO: more attributes
+	if (*attribute == "exposure")
+	{
+		ovrCamera.SetCameraExposure((int)value);
+	}
+	else if (*attribute == "gain")
+	{
+		ovrCamera.SetCameraGain((int)value);
+	}
+	else
+	{
+		// TODO: throw warning about unkown attribute
+	}
+}
+
+
+extern "C" DllExport void WriteTexture(unsigned char *leftUnityPtr, unsigned char *rightUnityPtr)
+{
+	if (ovrCamera.isOpen())
+	{
+		ovrCamera.PreStoreCamData(OVR::OV_CAMQT_DMS);
+
+		unsigned char *leftImg = ovrCamera.GetCamImageBGRA(OVR::OV_CAMEYE_LEFT);
+		unsigned char *rightImg = ovrCamera.GetCamImageBGRA(OVR::OV_CAMEYE_RIGHT);
+
+		auto imgSize = camHeight * camWidth * 4; // RGleftUnityPtrBA -> 4 channels
+		// possible memory leaks, only for internal use!
+		memcpy_s(leftUnityPtr, imgSize, leftImg, imgSize);
+		memcpy_s(rightUnityPtr, imgSize, rightUnityPtr, imgSize);
+	}
+}
