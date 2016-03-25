caa5aa454d925a45b104528176fe9b3559c4fe90
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2016-03-25 23:16:37 +0100

    Clean up external projects
    Former-commit-id: ba9df4f7501115dfadba4fa98e517fc0d368800f

--- external/ImageProcessingTest/Init.cs
+++ external/ImageProcessingTest/Init.cs
@@ -1,43 +1,12 @@
-using Emgu.CV;
-using Emgu.CV.CvEnum;
-using Emgu.CV.Structure;
-using ImageProcessingUtil;
 using System.Diagnostics;
 
-namespace ImageProcessingTest
+namespace ImageProcessingUtil
 {
-
-
-
     class Init
     {
         static void Main(string[] args)
         {
-            //using (var capture = new Capture(0))
-            //{
-            //    capture.SetCaptureProperty(CapProp.FrameWidth, 1280);
-            //    capture.SetCaptureProperty(CapProp.FrameHeight, 720);
-
-            //    while (true)
-            //    {
-            //        // in
-            //        var frame = capture.QueryFrame();
-            //        var watch = Stopwatch.StartNew();
-
-            //        var pose = new MarshalledPose();
-            //        var resultImg = frame.ToImage<Bgr, byte>();
-            //        ImageProcessor.TrackMarker(ref resultImg, ref pose);
-
-            //        watch.Stop();
-            //        //Console.Out.WriteLine(String.Format("{0,5:0.0}, {1,5:0.0}, {2,5:0.0}, {3,5:0.0}, {4,5:0.0}, {5,5:0.0}, took {6} ms", pose[0], pose[1], pose[2], pose[3], pose[4], pose[5], watch.ElapsedMilliseconds));
-
-            //        CvInvoke.Imshow("bla", resultImg);
-            //        CvInvoke.WaitKey(30);
-            //    }
-            //}
-
-
-            ImageProcessor.TestOvr();
+            // Test things without unity
         }
     }
 }
