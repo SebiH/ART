abea3a30c60e2f5f9512f059708a8361401f7aa7
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2016-03-27 17:18:38 +0200

    Write texture into unity ptr (WIP commit)
    Former-commit-id: 000e09231350fb5ecdf6cfe1009b002d8305201d

---
+++ Assets/Scripts/Util/OvrCameraFeed.cs
@@ -0,0 +1,159 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Util
+{
+    class OvrCameraFeed : MonoBehaviour
+    {
+        [DllImport("ImageProcessing")]
+        static extern void OvrStart(int cameraMode = -1);
+
+        [DllImport("ImageProcessing")]
+        static extern void OvrStop();
+
+        [DllImport("ImageProcessing")]
+        static extern float GetProperty(string prop);
+
+        [DllImport("ImageProcessing")]
+        static extern void SetProperty(string prop, float val);
+
+        [DllImport("ImageProcessing")]
+        static extern void WriteTexture(IntPtr leftUnityPtr, IntPtr rightUnityPtr);
+
+        // Camera GameObject
+        private GameObject CameraLeft;
+        private GameObject CameraRight;
+        private GameObject CameraPlaneLeft;
+        private GameObject CameraPlaneRight;
+        // Camera texture
+        private Texture2D CameraTexLeft = null;
+        private Texture2D CameraTexRight = null;
+        private Vector3 CameraRightGap;
+
+        private int ImageWidth;
+        private int ImageHeight;
+
+        private const float IMAGE_ZOFFSET = 0.02f;
+
+        void Awake()
+        {
+            OvrStart();
+            ImageWidth = (int)GetProperty("width");
+            ImageHeight = (int)GetProperty("height");
+        }
+
+
+        void Start()
+        {
+            // Initialize camera plane object(Left)
+            CameraLeft = transform.FindChild("LeftCamera").gameObject;
+            CameraRight = transform.FindChild("RightCamera").gameObject;
+            CameraPlaneLeft = CameraLeft.transform.FindChild("LeftImagePlane").gameObject;
+            CameraPlaneRight = CameraRight.transform.FindChild("RightImagePlane").gameObject;
+
+            CameraLeft.transform.localPosition = Vector3.zero;
+            CameraRight.transform.localPosition = Vector3.zero;
+            CameraLeft.transform.localRotation = Quaternion.identity;
+            CameraRight.transform.localRotation = Quaternion.identity;
+
+            // Create cam texture
+            CameraTexLeft = new Texture2D(ImageWidth, ImageHeight, TextureFormat.BGRA32, false);
+            CameraTexRight = new Texture2D(ImageWidth, ImageHeight, TextureFormat.BGRA32, false);
+            // Cam setting
+            CameraTexLeft.wrapMode = TextureWrapMode.Clamp;
+            CameraTexRight.wrapMode = TextureWrapMode.Clamp;
+
+            // Mesh
+            Mesh m = CreateCameraPlaneMesh();
+            CameraPlaneLeft.GetComponent<MeshFilter>().mesh = m;
+            CameraPlaneRight.GetComponent<MeshFilter>().mesh = m;
+
+            // SetShader
+            CameraLeft.GetComponent<Camera>().enabled = true;
+            CameraRight.GetComponent<Camera>().enabled = true;
+
+            CameraPlaneLeft.GetComponent<Renderer>().material.shader = Shader.Find("Ovrvision/ovTexture");
+            CameraPlaneRight.GetComponent<Renderer>().material.shader = Shader.Find("Ovrvision/ovTexture");
+
+            CameraPlaneLeft.GetComponent<Renderer>().materials[0].SetTexture("_MainTex", CameraTexLeft);
+            CameraPlaneRight.GetComponent<Renderer>().materials[0].SetTexture("_MainTex", CameraTexRight);
+            CameraPlaneLeft.GetComponent<Renderer>().materials[1].SetTexture("_MainTex", CameraTexLeft);
+            CameraPlaneRight.GetComponent<Renderer>().materials[1].SetTexture("_MainTex", CameraTexRight);
+
+            var defaultFloatpoint = 0.427990019f;
+            var defaultRightGap = new Vector3(0.0566581376f, -0.000236578562f, 0.001237078f);
+            var defaultAspectW = 1.0105263f;
+
+            CameraRightGap = defaultRightGap;
+
+            CameraPlaneLeft.transform.localScale = new Vector3(defaultAspectW, -1.0f, 1.0f);
+            CameraPlaneRight.transform.localScale = new Vector3(defaultAspectW, -1.0f, 1.0f);
+            CameraPlaneLeft.transform.localPosition = new Vector3(-0.032f, 0.0f, defaultFloatpoint + IMAGE_ZOFFSET);
+            CameraPlaneRight.transform.localPosition = new Vector3(CameraRightGap.x - 0.040f, 0.0f, defaultFloatpoint + IMAGE_ZOFFSET);
+
+            leftTexturePtr = CameraTexLeft.GetNativeTexturePtr();
+            rightTexturePtr = CameraTexRight.GetNativeTexturePtr();
+
+        }
+
+        private IntPtr leftTexturePtr, rightTexturePtr;
+
+        void Update()
+        {
+            if (leftTexturePtr != null && rightTexturePtr != null)
+            {
+                if (GetProperty("isOpen") >= 1.0f)
+                {
+                    WriteTexture(leftTexturePtr, rightTexturePtr);
+                }
+                else
+                {
+                    Debug.LogError("Camera is not open!");
+                }
+            }
+        }
+
+        void OnDestroy()
+        {
+            OvrStop();
+        }
+
+        private Mesh CreateCameraPlaneMesh()
+        {
+            Mesh m = new Mesh();
+            m.name = "CameraImagePlane";
+            Vector3[] vertices = new Vector3[]
+            {
+            new Vector3(-0.5f, -0.5f, 0.0f),
+            new Vector3( 0.5f,  0.5f, 0.0f),
+            new Vector3( 0.5f, -0.5f, 0.0f),
+            new Vector3(-0.5f,  0.5f, 0.0f)
+            };
+            int[] triangles = new int[]
+            {
+            0, 1, 2,
+            1, 0, 3
+            };
+            Vector2[] uv = new Vector2[]
+            {
+            new Vector2(0.0f, 0.0f),
+            new Vector2(1.0f, 1.0f),
+            new Vector2(1.0f, 0.0f),
+            new Vector2(0.0f, 1.0f)
+            };
+            m.vertices = vertices;
+            m.subMeshCount = 2;
+            m.SetTriangles(triangles, 0);
+            m.SetTriangles(triangles, 1);
+            m.uv = uv;
+            m.RecalculateNormals();
+
+            return m;
+        }
+
+    }
+}
--- external/ImageProcessing/api.cpp
+++ external/ImageProcessing/api.cpp
@@ -2,7 +2,7 @@
 
 #define DllExport   __declspec( dllexport )
 
-#include <memory>
+#include <d3d11.h>
 #include <opencv2/opencv.hpp>
 #include <ovrvision_pro.h>
 
@@ -11,7 +11,7 @@ using namespace cv;
 OVR::OvrvisionPro ovrCamera;
 int camWidth, camHeight;
 
-extern "C" DllExport void Start(int cameraMode = -1)
+extern "C" DllExport void OvrStart(int cameraMode = -1)
 {
 	OVR::Camprop camProp = (cameraMode == -1) ? OVR::OV_CAMVR_FULL : (OVR::Camprop)cameraMode;
 	ovrCamera = OVR::OvrvisionPro();
@@ -31,14 +31,19 @@ extern "C" DllExport void Start(int cameraMode = -1)
 }
 
 
-extern "C" DllExport void Stop()
+extern "C" DllExport void OvrStop()
 {
-	ovrCamera.Close();
+	if (ovrCamera.isOpen())
+	{
+		ovrCamera.Close();
+	}
 }
 
 
-extern "C" DllExport float GetProperty(const char *prop)
+extern "C" DllExport float GetProperty(const char *name)
 {
+	std::string prop(name);
+
 	if (prop == "width")
 	{
 		return (float)ovrCamera.GetCamWidth();
@@ -55,18 +60,24 @@ extern "C" DllExport float GetProperty(const char *prop)
 	{
 		return (float)ovrCamera.GetCameraGain();
 	}
+	else if (prop == "isOpen")
+	{
+		return (ovrCamera.isOpen()) ? 10.0f : 0.0f; // TODO: better return value?
+	}
 	else
 	{
 		cv::Mat test(cv::Size(200, 500), CV_64F);
 		imshow("testwin", test);
 		// TODO: throw warning about unknown prop
-		return .05f;
+		return -1.f;
 	}
 }
 
 
-extern "C" DllExport void SetProperty(const char *prop, float value)
+extern "C" DllExport void SetProperty(const char *name, float value)
 {
+	std::string prop(name);
+
 	// TODO: more props
 	if (prop == "exposure")
 	{
@@ -82,6 +93,40 @@ extern "C" DllExport void SetProperty(const char *prop, float value)
 	}
 }
 
+static float g_Time = 0.0f;
+
+static void FillTextureFromCode(int width, int height, int stride, unsigned char* dst)
+{
+	const float t = g_Time * 4.0f;
+	g_Time += 0.1f;
+
+	for (int y = 0; y < height; ++y)
+	{
+		unsigned char* ptr = dst;
+		for (int x = 0; x < width; ++x)
+		{
+			// Simple oldskool "plasma effect", a bunch of combined sine waves
+			int vv = int(
+				(127.0f + (127.0f * sinf(x / 7.0f + t))) +
+				(127.0f + (127.0f * sinf(y / 5.0f - t))) +
+				(127.0f + (127.0f * sinf((x + y) / 6.0f - t))) +
+				(127.0f + (127.0f * sinf(sqrtf(float(x*x + y*y)) / 4.0f - t)))
+				) / 4;
+
+			// Write the texture pixel
+			ptr[0] = vv;
+			ptr[1] = vv;
+			ptr[2] = vv;
+			ptr[3] = vv;
+
+			// To next pixel (our pixels are 4 bpp)
+			ptr += 4;
+		}
+
+		// To next image row
+		dst += stride;
+	}
+}
 
 extern "C" DllExport void WriteTexture(unsigned char *leftUnityPtr, unsigned char *rightUnityPtr)
 {
@@ -92,9 +137,65 @@ extern "C" DllExport void WriteTexture(unsigned char *leftUnityPtr, unsigned cha
 		unsigned char *leftImg = ovrCamera.GetCamImageBGRA(OVR::OV_CAMEYE_LEFT);
 		unsigned char *rightImg = ovrCamera.GetCamImageBGRA(OVR::OV_CAMEYE_RIGHT);
 
-		auto imgSize = camHeight * camWidth * 4; // RGleftUnityPtrBA -> 4 channels
-		// possible memory leaks, only for internal use!
-		memcpy_s(leftUnityPtr, imgSize, leftImg, imgSize);
-		memcpy_s(rightUnityPtr, imgSize, rightUnityPtr, imgSize);
+		ID3D11Texture2D* d3dtex = (ID3D11Texture2D*)leftUnityPtr;
+		ID3D11Device *g_D3D11Device;
+		d3dtex->GetDevice(&g_D3D11Device);
+
+		float phi = g_Time;
+		float cosPhi = cosf(phi);
+		float sinPhi = sinf(phi);
+
+		float worldMatrix[16] = {
+			cosPhi,-sinPhi,0,0,
+			sinPhi,cosPhi,0,0,
+			0,0,1,0,
+			0,0,0.7f,1,
+		};
+
+		ID3D11DeviceContext* ctx = NULL;
+		g_D3D11Device->GetImmediateContext(&ctx);
+
+		//// update constant buffer - just the world matrix in our case
+		//ctx->UpdateSubresource(g_D3D11CB, 0, NULL, worldMatrix, 64, 0);
+
+		//// set shaders
+		//ctx->VSSetConstantBuffers(0, 1, &g_D3D11CB);
+		//ctx->VSSetShader(g_D3D11VertexShader, NULL, 0);
+		//ctx->PSSetShader(g_D3D11PixelShader, NULL, 0);
+
+		//// update vertex buffer
+		//ctx->UpdateSubresource(g_D3D11VB, 0, NULL, verts, sizeof(verts[0]) * 3, 0);
+
+		//// set input assembler data and draw
+		//ctx->IASetInputLayout(g_D3D11InputLayout);
+		//ctx->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+		//UINT stride = sizeof(MyVertex);
+		//UINT offset = 0;
+		//ctx->IASetVertexBuffers(0, 1, &g_D3D11VB, &stride, &offset);
+		//ctx->Draw(3, 0);
+
+		// update native texture from code
+		//if (g_TexturePointer)
+		{
+			//ID3D11Texture2D* d3dtex = (ID3D11Texture2D*)g_TexturePointer;
+			D3D11_TEXTURE2D_DESC desc;
+			d3dtex->GetDesc(&desc);
+
+			//unsigned char* data = new unsigned char[desc.Width*desc.Height * 4];
+			//FillTextureFromCode(desc.Width, desc.Height, desc.Width * 4, data);
+			//ctx->UpdateSubresource(d3dtex, 0, NULL, data, desc.Width * 4, 0);
+
+			//delete[] data;
+
+
+
+			ctx->UpdateSubresource(d3dtex, 0, NULL, leftImg, desc.Width * 4, 0);
+			cv::Mat mat(camHeight, camWidth, CV_8UC4, leftImg);
+			imshow("xyz", mat);
+			cv::waitKey(10);
+		}
+
+		ctx->Release();
+
 	}
 }
