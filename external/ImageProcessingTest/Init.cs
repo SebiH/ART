6f308300b699b0edfc827cde576b3f513e406b11
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2016-04-08 15:11:40 +0200

    Add code for StereoCalibration using OVR cameras

--- external/ImageProcessingTest/Init.cs
+++ external/ImageProcessingTest/Init.cs
@@ -1,12 +1,17 @@
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace ImageProcessingUtil
 {
     class Init
     {
+        [DllImport("ImageProcessing")]
+        private static extern void StereoCalibration();
+
         static void Main(string[] args)
         {
             // Test things without unity
+            StereoCalibration();
         }
     }
 }
