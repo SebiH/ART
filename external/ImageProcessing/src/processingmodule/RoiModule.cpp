8438d8b165cf779ccbe15f1853c9e96fb0dec157
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2016-04-13 18:30:59 +0200

    Fix swapped height/width, unassigned Mat

--- external/ImageProcessing/src/processingmodule/RoiModule.cpp
+++ external/ImageProcessing/src/processingmodule/RoiModule.cpp
@@ -24,13 +24,13 @@ std::vector<ProcessingOutput> RoiModule::processImage(unsigned char * rawDataLef
 		roi = _roi;
 	}
 
-	cv::Mat rawMatLeft = cv::Mat(_maxWidth, _maxHeight, CV_8UC4, rawDataLeft);
+	cv::Mat rawMatLeft = cv::Mat(cv::Size(_maxWidth, _maxHeight), CV_8UC4, rawDataLeft);
 	cv::Mat rawRoiLeft = cv::Mat(rawMatLeft, roi);
 	cv::Mat pureRoiLeft;
 	rawRoiLeft.copyTo(pureRoiLeft);
 
 
-	cv::Mat rawMatRight = cv::Mat(_maxWidth, _maxHeight, CV_8UC4, rawDataRight);
+	cv::Mat rawMatRight = cv::Mat(cv::Size(_maxWidth, _maxHeight), CV_8UC4, rawDataRight);
 	cv::Mat rawRoiRight = cv::Mat(rawMatRight, roi);
 	cv::Mat pureRoiRight;
 	rawRoiRight.copyTo(pureRoiRight);
@@ -42,11 +42,13 @@ std::vector<ProcessingOutput> RoiModule::processImage(unsigned char * rawDataLef
 	ProcessingOutput outputLeft;
 	outputLeft.type = ProcessingOutput::Type::left;
 	outputLeft.data = std::unique_ptr<unsigned char[]>(new unsigned char[memSize]);
+	outputLeft.img = pureRoiLeft;
 	memcpy(outputLeft.data.get(), rawRoiLeft.data, memSize);
 
 	ProcessingOutput outputRight;
 	outputRight.type = ProcessingOutput::Type::right;
 	outputRight.data = std::unique_ptr<unsigned char[]>(new unsigned char[memSize]);
+	outputRight.img = pureRoiRight;
 	memcpy(outputRight.data.get(), rawRoiRight.data, memSize);
 
 	std::vector<ProcessingOutput> output;
