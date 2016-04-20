f501d86d5156b17f44b6a2d1b50eb57670e8550a
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2016-04-20 20:04:13 +0200

    Add script to create (empty) window from double-pinch gesture

---
+++ Assets/Scripts/Actions/CreateWindow.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using Assets.Scripts.GestureControl;
+using Assets.Scripts.Gestures;
+
+public class CreateWindow : MonoBehaviour
+{
+    public int MinWidth = 50;
+    public int MinHeight = 50;
+
+    private GameObject _createdWindow;
+
+    public void StartCreation(GestureEventArgs e)
+    {
+        var gesture = e.Sender as IPositionGesture;
+        
+        if (gesture != null)
+        {
+            _createdWindow = GameObject.CreatePrimitive(PrimitiveType.Plane);
+
+            _createdWindow.transform.position = gesture.GetGesturePosition(Hand.Both);
+            _createdWindow.transform.localScale = GetScale(gesture);
+            _createdWindow.transform.rotation = Quaternion.Euler(GetRotation(gesture));
+        }
+    }
+
+    public void UpdateCreation(GestureEventArgs e)
+    {
+        var gesture = e.Sender as IPositionGesture;
+
+        if (gesture != null && _createdWindow != null)
+        {
+            _createdWindow.transform.position = gesture.GetGesturePosition(Hand.Both);
+            _createdWindow.transform.localScale = GetScale(gesture);
+            _createdWindow.transform.rotation = Quaternion.Euler(GetRotation(gesture));
+        }
+    }
+
+    public void FinishCreation(GestureEventArgs e)
+    {
+        _createdWindow = null;
+    }
+
+
+    private Vector3 GetScale(IPositionGesture gesture)
+    {
+        var gesturePosLeft = gesture.GetGesturePosition(Hand.Left);
+        var gesturePosRight = gesture.GetGesturePosition(Hand.Right);
+
+        var MinScale = 0.001f;
+        var MaxScale = 1;
+        var scale = gesturePosLeft - gesturePosRight;
+        var scaleX = Mathf.Clamp(Mathf.Abs(scale.x), MinScale, MaxScale) / 10f;
+        var scaleY = 1;
+        var scaleZ = Mathf.Clamp(Mathf.Abs(scale.y), MinScale, MaxScale) / 10f;
+
+        return new Vector3(scaleX, scaleY, scaleZ);
+    }
+
+    private Vector3 GetRotation(IPositionGesture gesture)
+    {
+        var gesturePosLeft = gesture.GetGesturePosition(Hand.Left);
+        var gesturePosRight = gesture.GetGesturePosition(Hand.Right);
+
+        var delta = gesturePosLeft - gesturePosRight;
+        var rad = Mathf.Atan2(delta.z, delta.x);
+        var deg = rad * (180f / Mathf.PI);
+
+        return new Vector3(270, -deg, 0);
+    }
+}
