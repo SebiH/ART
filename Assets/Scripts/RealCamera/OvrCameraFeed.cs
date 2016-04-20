6c9c0b92ca8dc00e4031da4b868ea4af1e8c6d2a
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2016-04-20 20:56:57 +0200

    Allow alphachannel modifications for camera-feed

--- Assets/Scripts/RealCamera/OvrCameraFeed.cs
+++ Assets/Scripts/RealCamera/OvrCameraFeed.cs
@@ -6,17 +6,13 @@ namespace Assets.Scripts.RealCamera
     class OvrCameraFeed : MonoBehaviour
     {
         // Camera GameObject
-        private GameObject CameraLeft;
-        private GameObject CameraRight;
         private GameObject CameraPlaneLeft;
         private GameObject CameraPlaneRight;
+
         // Camera texture
         // TODO: should be rendertexture??
         private Texture2D CameraTexLeft = null;
         private Texture2D CameraTexRight = null;
-        private Vector3 CameraRightGap;
-        private IntPtr LeftTexturePtr;
-        private IntPtr RightTexturePtr;
 
         private int ImageWidth;
         private int ImageHeight;
@@ -37,8 +33,8 @@ namespace Assets.Scripts.RealCamera
         void Start()
         {
             // Initialize camera plane object(Left)
-            CameraLeft = transform.FindChild("LeftCamera").gameObject;
-            CameraRight = transform.FindChild("RightCamera").gameObject;
+            var CameraLeft = transform.FindChild("LeftCamera").gameObject;
+            var CameraRight = transform.FindChild("RightCamera").gameObject;
             CameraPlaneLeft = CameraLeft.transform.FindChild("LeftImagePlane").gameObject;
             CameraPlaneRight = CameraRight.transform.FindChild("RightImagePlane").gameObject;
 
@@ -60,31 +56,22 @@ namespace Assets.Scripts.RealCamera
             CameraPlaneLeft.GetComponent<MeshFilter>().mesh = m;
             CameraPlaneRight.GetComponent<MeshFilter>().mesh = m;
 
-            // SetShader
-            CameraLeft.GetComponent<Camera>().enabled = true;
-            CameraRight.GetComponent<Camera>().enabled = true;
-
-            CameraPlaneLeft.GetComponent<Renderer>().material.shader = Shader.Find("Ovrvision/ovTexture");
-            CameraPlaneRight.GetComponent<Renderer>().material.shader = Shader.Find("Ovrvision/ovTexture");
-
             CameraPlaneLeft.GetComponent<Renderer>().materials[0].SetTexture("_MainTex", CameraTexLeft);
             CameraPlaneRight.GetComponent<Renderer>().materials[0].SetTexture("_MainTex", CameraTexRight);
-            CameraPlaneLeft.GetComponent<Renderer>().materials[1].SetTexture("_MainTex", CameraTexLeft);
-            CameraPlaneRight.GetComponent<Renderer>().materials[1].SetTexture("_MainTex", CameraTexRight);
 
             var defaultFloatpoint = 0.427990019f;
             var defaultRightGap = new Vector3(0.0566581376f, -0.000236578562f, 0.001237078f);
             var defaultAspectW = 1.0105263f;
 
-            CameraRightGap = defaultRightGap;
+            var CameraRightGap = defaultRightGap;
 
             CameraPlaneLeft.transform.localScale = new Vector3(defaultAspectW, -1.0f, 1.0f);
             CameraPlaneRight.transform.localScale = new Vector3(defaultAspectW, -1.0f, 1.0f);
             CameraPlaneLeft.transform.localPosition = new Vector3(-0.032f, 0.0f, defaultFloatpoint + IMAGE_ZOFFSET);
             CameraPlaneRight.transform.localPosition = new Vector3(CameraRightGap.x - 0.040f, 0.0f, defaultFloatpoint + IMAGE_ZOFFSET);
 
-            LeftTexturePtr = CameraTexLeft.GetNativeTexturePtr();
-            RightTexturePtr = CameraTexRight.GetNativeTexturePtr();
+            var LeftTexturePtr = CameraTexLeft.GetNativeTexturePtr();
+            var RightTexturePtr = CameraTexRight.GetNativeTexturePtr();
 
             // TODO: store handle for deregister
             TextureHandleLeft = ImageProcessing.AddTexturePtr(ImageProcessing.MODULE_RAW_IMAGE, LeftTexturePtr, ImageProcessing.Type.left);
@@ -93,6 +80,12 @@ namespace Assets.Scripts.RealCamera
             // quick hack to stop unity from crashing :(
             ImageProcessing.AddTextureSync(CameraTexLeft);
             ImageProcessing.AddTextureSync(CameraTexRight);
+        }
+
+        public void SetAlpha(float alpha)
+        {
+            CameraPlaneLeft.GetComponent<Renderer>().materials[0].color = new Color(1, 1, 1, alpha);
+            CameraPlaneRight.GetComponent<Renderer>().materials[0].color = new Color(1, 1, 1, alpha);
         }
 
         void OnDestroy()
@@ -128,7 +121,7 @@ namespace Assets.Scripts.RealCamera
             new Vector2(0.0f, 1.0f)
             };
             m.vertices = vertices;
-            m.subMeshCount = 2;
+            m.subMeshCount = 3;
             m.SetTriangles(triangles, 0);
             m.SetTriangles(triangles, 1);
             m.uv = uv;
