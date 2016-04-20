57f5739be06b7ad126936bdf82ba3fbf0506ac43
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2016-04-20 16:31:02 +0200

    Add finger-base as interaction limbs

--- Assets/Code/Gesture/InteractionLimb.cs
+++ Assets/Code/Gesture/InteractionLimb.cs
@@ -3,18 +3,28 @@ namespace Assets.Scripts.GestureControl
     public enum InteractionLimb
     {
         LeftThumbTip,
+        LeftThumbBase,
         LeftIndexTip,
+        LeftIndexBase,
         LeftMiddleTip,
+        LeftMiddleBase,
         LeftRingTip,
+        LeftRingBase,
         LeftLittleTip,
+        LeftLittleBase,
 
         LeftPalm,
 
         RightThumbTip,
+        RightThumbBase,
         RightIndexTip,
+        RightIndexBase,
         RightMiddleTip,
+        RightMiddleBase,
         RightRingTip,
+        RightRingBase,
         RightLittleTip,
+        RightLittleBase,
 
         RightPalm
     }
