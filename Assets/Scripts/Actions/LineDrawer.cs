178ca6c30aef17bec13f19d2cc91f50927e25bb3
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2016-04-21 11:28:29 +0200

    Draw lines on Pinch Gesture

---
+++ Assets/Scripts/Actions/LineDrawer.cs
@@ -0,0 +1,45 @@
+using Assets.Scripts.GestureControl;
+using Assets.Scripts.Gestures;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineDrawer : MonoBehaviour
+{
+    private List<Vector3> _positions = new List<Vector3>();
+
+    public void StartDrawing(GestureEventArgs e)
+    {
+        _positions.Clear();
+
+        var gesture = e.Sender as IPositionGesture;
+
+        if (gesture != null)
+        {
+            _positions.Add(gesture.GetGesturePosition(Hand.Either));
+        }
+    }
+
+    public void UpdateDrawing(GestureEventArgs e)
+    {
+        var gesture = e.Sender as IPositionGesture;
+
+        if (gesture != null)
+        {
+            _positions.Add(gesture.GetGesturePosition(Hand.Either));
+        }
+    }
+
+    public void EndDrawing(GestureEventArgs e)
+    {
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        for (int i = 1; i < _positions.Count; i++)
+        {
+            Gizmos.DrawLine(_positions[i - 1], _positions[i]);
+        }
+    }
+
+}
