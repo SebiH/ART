77418822e62680a3c3e10d699fd9e36340dfc73f
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2016-06-10 18:42:15 +0200

    Add (empty) Unicast socket class for Unity/Optitrack connections

---
+++ Assets/ThirdParty/UnityOptitrack/DirectUnicastSocketClient.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OptitrackManagement
+{
+    class DirectUnicastSocketClient : IOptitrackSocket
+    {
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataStream GetDataStream()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsInit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
