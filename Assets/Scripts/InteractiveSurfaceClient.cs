a26f61b5ae64aaea81bd6e31e0f051e5ecd2b557
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2016-07-23 12:28:36 +0200

    Add crude network connection to display cursor position on InteractiveSurface in Unity

---
+++ Assets/Scripts/InteractiveSurfaceClient.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Net.Sockets;
+using System.Threading;
+using UnityEngine;
+
+public class InteractiveSurfaceClient : MonoBehaviour
+{
+    public string ServerIp = "127.0.0.1";
+    public int ServerPort = 8835;
+
+    public GameObject Screen;
+    public GameObject Cursor;
+
+    private TcpClient _client;
+    private bool _isRunning;
+    private Vector2 _currentPosition = Vector2.zero;
+
+    void Start()
+    {
+        _client = new TcpClient();
+        _client.Connect(ServerIp, ServerPort);
+        _isRunning = true;
+
+        Thread t = new Thread(new ThreadStart(ReceiveData));
+        t.Start();
+    }
+
+    private void ReceiveData()
+    {
+        var stream = _client.GetStream();
+
+        var expectedFloats = 2;
+        var byteBuffer = new byte[expectedFloats * sizeof(float)];
+        var floatBuffer = new float[expectedFloats];
+
+        while (_isRunning)
+        {
+            stream.Read(byteBuffer, 0, byteBuffer.Length);
+            Buffer.BlockCopy(byteBuffer, 0, floatBuffer, 0, byteBuffer.Length);
+            _currentPosition = new Vector2(floatBuffer[0], floatBuffer[1]);
+        }
+    }
+
+    void Update()
+    {
+        Cursor.transform.localPosition = new Vector3(_currentPosition.x - 0.5f, 0, _currentPosition.y - 0.5f);
+    }
+
+    void OnDestroy()
+    {
+        _isRunning = false;
+        _client.Close();
+    }
+}
--- external/InteractiveDisplay/MainWindow.xaml.cs
+++ external/InteractiveDisplay/MainWindow.xaml.cs
@@ -1,28 +1,102 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace InteractiveDisplay
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private TcpListener _server;
+        private List<Socket> _clients = new List<Socket>();
+
+        public class StateObject
+        {
+            public const int Size = 1024 * 1024;
+            public byte[] Buffer = new byte[Size];
+        }
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _server = new TcpListener(IPAddress.Any, 8835);
+            _server.Start();
+            bool _isRunning = true;
+
+            Task.Factory.StartNew(() =>
+            {
+                while (_isRunning)
+                {
+                    var socket = _server.AcceptSocket();
+                    lock (_clients)
+                    {
+                        _clients.Add(socket);
+                    }
+
+                    Task.Factory.StartNew(() =>
+                    {
+                        try
+                        {
+                            while (true)
+                            {
+                                // we're not expecting any packages
+                                socket.Receive(new byte[1]);
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            lock (_clients)
+                            {
+                                _clients.Remove(socket);
+                            }
+                        }
+                    });
+                }
+            });
+
+
+            Unloaded += (s, e) =>
+            {
+                _isRunning = false;
+                _server.Stop();
+            };
+        }
+
+        private void Window_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            SendCoordinates(e.GetPosition(this));
+        }
+
+        private void Window_TouchMove(object sender, System.Windows.Input.TouchEventArgs e)
+        {
+            var touchPoint = e.GetTouchPoint(this);
+            SendCoordinates(touchPoint.Position);
+        }
+
+        private void SendCoordinates(Point coords)
+        {
+            var rawFloats = new float[] { (float)(coords.X / ActualWidth), (float)(coords.Y / ActualHeight) };
+            var rawBytes = new byte[rawFloats.Length * sizeof(float)];
+            Buffer.BlockCopy(rawFloats, 0, rawBytes, 0, rawBytes.Length);
+
+            lock (_clients)
+            {
+                foreach (var client in _clients)
+                {
+                    try
+                    {
+                        client.Send(rawBytes);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
         }
     }
 }
