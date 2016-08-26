61a54e5dc63d105faa624f25763c269b6067c182
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2016-08-26 20:39:17 +0200

    Implement OpenCvOutput

---
+++ imageprocessing/ImageProcessing/src/outputs/OpenCvOutput.cpp
@@ -0,0 +1,40 @@
+#include "outputs/OpenCvOutput.h"
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace ImageProcessing;
+
+OpenCvOutput::OpenCvOutput(std::string windowname)
+	: windowname_(windowname)
+{
+	cv::namedWindow(windowname);
+}
+
+OpenCvOutput::~OpenCvOutput()
+{
+	cv::destroyWindow(windowname_);
+}
+
+
+void OpenCvOutput::RegisterResult(const FrameData &result)
+{
+	Output::RegisterResult(result);
+
+	// write result immediately
+	Write(result);
+}
+
+void OpenCvOutput::Write(const FrameData &result)
+{
+	int totalWidth = 0;
+	int maxHeight = 0;
+	int matType = -1;
+
+	cv::Mat merged(cv::Size(result.size.width * 2, result.size.height), matType);
+
+	cv::Mat left(merged, cv::Rect(cv::Point(0, 0), cv::Size(result.size.width, result.size.height)));
+	cv::Mat right(merged, cv::Rect(cv::Point(result.size.width, 0), cv::Size(result.size.width, result.size.height)));
+
+	imshow(windowname_, merged);
+}
---
+++ imageprocessing/ImageProcessing/src/outputs/OpenCvOutput.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+#include "outputs/Output.h"
+
+namespace ImageProcessing
+{
+	class OpenCvOutput : public Output
+	{
+	private:
+		std::string windowname_;
+
+	public:
+		OpenCvOutput(std::string windowname);
+		~OpenCvOutput();
+
+		void RegisterResult(const FrameData &result) override;
+
+	protected:
+		virtual void Write(const FrameData &frame) noexcept override;
+	};
+}
