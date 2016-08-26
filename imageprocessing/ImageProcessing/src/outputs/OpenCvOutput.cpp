c04ad92cae2a2391f47281e4395ecbd08461daf9
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2016-08-26 23:21:05 +0200

    Fix opencv output not writing anything, not handling different depths

--- imageprocessing/ImageProcessing/src/outputs/OpenCvOutput.cpp
+++ imageprocessing/ImageProcessing/src/outputs/OpenCvOutput.cpp
@@ -27,10 +27,21 @@ void OpenCvOutput::RegisterResult(const FrameData &result)
 
 void OpenCvOutput::Write(const FrameData &result) noexcept
 {
-	cv::Mat merged(cv::Size(result.size.width * 2, result.size.height), CV_8UC4);
+	// TODO: get cv type from framedata...?
+	int cv_type;
+	if (result.size.depth == 1)      { cv_type = CV_8UC1; }
+	else if (result.size.depth == 3) { cv_type = CV_8UC3; }
+	else if (result.size.depth == 4) { cv_type = CV_8UC4; }
 
+	cv::Mat merged(cv::Size(result.size.width * 2, result.size.height), cv_type);
+
+	cv::Mat leftSrc(cv::Size(result.size.width, result.size.height), cv_type, result.buffer_left.get());
 	cv::Mat left(merged, cv::Rect(cv::Point(0, 0), cv::Size(result.size.width, result.size.height)));
-	cv::Mat right(merged, cv::Rect(cv::Point(result.size.width, 0), cv::Size(result.size.width, result.size.height)));
+	leftSrc.copyTo(left);
 
-	imshow(windowname_, merged);
+	cv::Mat rightSrc(cv::Size(result.size.width, result.size.height), cv_type, result.buffer_right.get());
+	cv::Mat right(merged, cv::Rect(cv::Point(result.size.width, 0), cv::Size(result.size.width, result.size.height)));
+	rightSrc.copyTo(right);
+
+	cv::imshow(windowname_, merged);
 }
