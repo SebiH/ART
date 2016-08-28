c6aec6621025e7f22ec4e9e26ea0b15d88c16549
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2016-08-28 21:03:13 +0200

    Make geting/setting camera properties more robust

--- imageprocessing/ImageProcessing/src/api/camera_properties.cpp
+++ imageprocessing/ImageProcessing/src/api/camera_properties.cpp
@@ -7,97 +7,219 @@
 
 extern "C" UNITY_INTERFACE_EXPORT int GetCamWidth()
 {
-	return ImageProcessing::ActiveCamera::Instance()->GetSource()->GetFrameWidth();
+	auto cam = ImageProcessing::ActiveCamera::Instance()->GetSource();
+	if (cam)
+	{
+		return cam->GetFrameWidth();
+	}
+	else
+	{
+		return 0;
+	}
 }
 
 extern "C" UNITY_INTERFACE_EXPORT int GetCamHeight()
 {
-	return ImageProcessing::ActiveCamera::Instance()->GetSource()->GetFrameHeight();
+	auto cam = ImageProcessing::ActiveCamera::Instance()->GetSource();
+
+	if (cam)
+	{
+		return cam->GetFrameHeight();
+	}
+	else
+	{
+		return 0;
+	}
 }
 
 extern "C" UNITY_INTERFACE_EXPORT int GetCamChannels()
 {
-	return ImageProcessing::ActiveCamera::Instance()->GetSource()->GetFrameChannels();
+	auto cam = ImageProcessing::ActiveCamera::Instance()->GetSource();
+	if (cam.get() != nullptr)
+	{
+		return cam->GetFrameChannels();
+	}
+	else
+	{
+		return 0;
+	}
 }
 
 extern "C" UNITY_INTERFACE_EXPORT int GetCamGain()
 {
-	return ImageProcessing::ActiveCamera::Instance()->GetSource()->GetCamGain();
+	auto cam = ImageProcessing::ActiveCamera::Instance()->GetSource();
+
+	if (cam)
+	{
+		return cam->GetCamGain();
+	}
+	else
+	{
+		return -1;
+	}
 }
 
 extern "C" UNITY_INTERFACE_EXPORT void SetCamGain(const int val)
 {
-	ImageProcessing::ActiveCamera::Instance()->GetSource()->SetCamGain(val);
+	auto cam = ImageProcessing::ActiveCamera::Instance()->GetSource();
+	if (cam)
+	{
+		cam->SetCamGain(val);
+	}
 }
 
 extern "C" UNITY_INTERFACE_EXPORT int GetCamExposure()
 {
-	return ImageProcessing::ActiveCamera::Instance()->GetSource()->GetCamExposure();
+	auto cam = ImageProcessing::ActiveCamera::Instance()->GetSource();
+	if (cam)
+	{
+		return cam->GetCamExposure();
+	}
+	else
+	{
+		return -1;
+	}
 }
 
 extern "C" UNITY_INTERFACE_EXPORT void SetCamExposure(const int val)
 {
-	ImageProcessing::ActiveCamera::Instance()->GetSource()->SetCamExposure(val);
+	auto cam = ImageProcessing::ActiveCamera::Instance()->GetSource();
+	if (cam)
+	{
+		cam->SetCamExposure(val);
+	}
 }
 
 extern "C" UNITY_INTERFACE_EXPORT void SetCamExposurePerSec(const float val)
 {
-	ImageProcessing::ActiveCamera::Instance()->GetSource()->SetCamExposurePerSec(val);
+	auto cam = ImageProcessing::ActiveCamera::Instance()->GetSource();
+	if (cam)
+	{
+		cam->SetCamExposurePerSec(val);
+	}
 }
 
 extern "C" UNITY_INTERFACE_EXPORT int GetCamBLC()
 {
-	return ImageProcessing::ActiveCamera::Instance()->GetSource()->GetCamBLC();
+	auto cam = ImageProcessing::ActiveCamera::Instance()->GetSource();
+	if (cam)
+	{
+		return cam->GetCamBLC();
+	}
+	else
+	{
+		return -1;
+	}
 }
 
 extern "C" UNITY_INTERFACE_EXPORT void SetCamBLC(const int val)
 {
-	ImageProcessing::ActiveCamera::Instance()->GetSource()->SetCamBLC(val);
+	auto cam = ImageProcessing::ActiveCamera::Instance()->GetSource();
+	if (cam)
+	{
+		cam->SetCamBLC(val);
+	}
 }
 
 extern "C" UNITY_INTERFACE_EXPORT bool GetCamAutoWhiteBalance()
 {
-	return ImageProcessing::ActiveCamera::Instance()->GetSource()->GetCamAutoWhiteBalance();
+	auto cam = ImageProcessing::ActiveCamera::Instance()->GetSource();
+	if (cam)
+	{
+		return cam->GetCamAutoWhiteBalance();
+	}
+	else
+	{
+		return false;
+	}
 }
 
 extern "C" UNITY_INTERFACE_EXPORT void SetCamAutoWhiteBalance(const bool val)
 {
-	ImageProcessing::ActiveCamera::Instance()->GetSource()->SetCamAutoWhiteBalance(val);
+	auto cam = ImageProcessing::ActiveCamera::Instance()->GetSource();
+	if (cam)
+	{
+		cam->SetCamAutoWhiteBalance(val);
+	}
 }
 
 extern "C" UNITY_INTERFACE_EXPORT int GetCamWhiteBalanceR()
 {
-	return ImageProcessing::ActiveCamera::Instance()->GetSource()->GetCamWhiteBalanceR();
+	auto cam = ImageProcessing::ActiveCamera::Instance()->GetSource();
+	if (cam)
+	{
+		return cam->GetCamWhiteBalanceR();
+	}
+	else
+	{
+		return -1;
+	}
 }
 
 extern "C" UNITY_INTERFACE_EXPORT void SetCamWhiteBalanceR(const int val)
 {
-	ImageProcessing::ActiveCamera::Instance()->GetSource()->SetCamWhiteBalanceR(val);
+	auto cam = ImageProcessing::ActiveCamera::Instance()->GetSource();
+	if (cam)
+	{
+		cam->SetCamWhiteBalanceR(val);
+	}
 }
 
 extern "C" UNITY_INTERFACE_EXPORT int GetCamWhiteBalanceG()
 {
-	return ImageProcessing::ActiveCamera::Instance()->GetSource()->GetCamWhiteBalanceG();
+	auto cam = ImageProcessing::ActiveCamera::Instance()->GetSource();
+	if (cam)
+	{
+		return cam->GetCamWhiteBalanceG();
+	}
+	else
+	{
+		return -1;
+	}
 }
 
 extern "C" UNITY_INTERFACE_EXPORT void SetCamWhiteBalanceG(const int val)
 {
-	ImageProcessing::ActiveCamera::Instance()->GetSource()->SetCamWhiteBalanceG(val);
+	auto cam = ImageProcessing::ActiveCamera::Instance()->GetSource();
+	if (cam)
+	{
+		cam->SetCamWhiteBalanceG(val);
+	}
 }
 
 extern "C" UNITY_INTERFACE_EXPORT int GetCamWhiteBalanceB()
 {
-	return ImageProcessing::ActiveCamera::Instance()->GetSource()->GetCamWhiteBalanceB();
+	auto cam = ImageProcessing::ActiveCamera::Instance()->GetSource();
+	if (cam)
+	{
+		return cam->GetCamWhiteBalanceB();
+	}
+	else
+	{
+		return -1;
+	}
 }
 
 extern "C" UNITY_INTERFACE_EXPORT void SetCamWhiteBalanceB(const int val)
 {
-	ImageProcessing::ActiveCamera::Instance()->GetSource()->SetCamWhiteBalanceB(val);
+	auto cam = ImageProcessing::ActiveCamera::Instance()->GetSource();
+	if (cam)
+	{
+		cam->SetCamWhiteBalanceB(val);
+	}
 }
 
 extern "C" UNITY_INTERFACE_EXPORT int GetCamFps()
 {
-	return ImageProcessing::ActiveCamera::Instance()->GetSource()->GetCamFps();
+	auto cam = ImageProcessing::ActiveCamera::Instance()->GetSource();
+	if (cam)
+	{
+		return cam->GetCamFps();
+	}
+	else
+	{
+		return -1;
+	}
 }
 
 extern "C" UNITY_INTERFACE_EXPORT float GetHMDRightGap(const int at)
