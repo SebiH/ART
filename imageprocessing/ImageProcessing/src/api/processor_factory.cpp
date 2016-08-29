7245edf44fe398c0a908c2939d0aba20417d6b1e
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2016-08-29 12:11:30 +0200

    Implement method to remove processors from pipeline

--- imageprocessing/ImageProcessing/src/api/processor_factory.cpp
+++ imageprocessing/ImageProcessing/src/api/processor_factory.cpp
@@ -4,9 +4,30 @@
 #include "processors/Processor.h"
 #include "pipelines/PipelineManager.h"
 
+using namespace ImageProcessing;
 
 extern "C" UNITY_INTERFACE_EXPORT int AddArToolkitProcessor(const int pipeline_id)
 {
 	// TODO.
 	return pipeline_id;
+}
+
+
+extern "C" UNITY_INTERFACE_EXPORT void RemoveProcessor(const int pipeline_id, const int processor_id)
+{
+	try
+	{
+		auto pipeline = PipelineManager::Instance()->GetPipeline(pipeline_id);
+		
+		if (!pipeline)
+		{
+			throw std::exception("No pipeline with this id");
+		}
+
+		pipeline->RemoveProcessor(processor_id);
+	}
+	catch (const std::exception &e)
+	{
+		DebugLog(std::string("Unable to remove processor: ") + e.what());
+	}
 }
