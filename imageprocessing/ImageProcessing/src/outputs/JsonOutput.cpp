58bfd96f313d63bb85dffd32d8e9c321e69ad77d
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2016-08-29 11:45:25 +0200

    Fix JsonOutput not writing immediately

--- imageprocessing/ImageProcessing/src/outputs/JsonOutput.cpp
+++ imageprocessing/ImageProcessing/src/outputs/JsonOutput.cpp
@@ -1,11 +1,12 @@
 #include "outputs/JsonOutput.h"
 
 #include "frames/JsonFrameData.h"
+#include "utils/Logger.h"
 
 using namespace ImageProcessing;
 
 JsonOutput::JsonOutput(JsonCallback &callback)
-	:callback_(callback)
+	: callback_(callback)
 {
 
 }
@@ -20,7 +21,7 @@ JsonOutput::~JsonOutput()
 void JsonOutput::RegisterResult(const FrameData &frame)
 {
 	// write result immediately, don't wait
-	Output::RegisterResult(frame);
+	Write(frame);
 }
 
 
@@ -34,6 +35,10 @@ void JsonOutput::Write(const FrameData &frame) noexcept
 	if (auto json_frame = dynamic_cast<const JsonFrameData *>(&frame))
 	{
 		callback_(json_frame->json.c_str());
+	}
+	else
+	{
+		DebugLog("Could not trigger callback: Provided frame does not have JSON!");
 	}
 }
 
