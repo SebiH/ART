5147396e9925bc2027cdcdb13d1308d24908b417
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2016-08-30 17:52:13 +0200

    Add method to extract OpenCV type from framesize

--- imageprocessing/ImageProcessing/src/frames/FrameSize.h
+++ imageprocessing/ImageProcessing/src/frames/FrameSize.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstddef>
+#include <opencv2/core.hpp>
 
 namespace ImageProcessing
 {
@@ -29,6 +30,23 @@ namespace ImageProcessing
 		std::size_t inline BufferSize() const
 		{
 			return width * height * depth;
+		}
+
+		int inline CvType() const
+		{
+			switch (depth)
+			{
+			case 1:
+				return CV_8UC1;
+			case 2:
+				return CV_8UC2;
+			case 3:
+				return CV_8UC3;
+			case 4:
+				return CV_8UC4;
+			default:
+				throw std::exception("Invalid depth!");
+			}
 		}
 
 		bool operator ==(const FrameSize &other) const
--- imageprocessing/ImageProcessing/src/outputs/OpenCvOutput.cpp
+++ imageprocessing/ImageProcessing/src/outputs/OpenCvOutput.cpp
@@ -28,19 +28,13 @@ void OpenCvOutput::RegisterResult(const std::shared_ptr<const FrameData> &result
 void OpenCvOutput::Write(const FrameData *result) noexcept
 {
 	// TODO: get cv type from framedata...?
-	int cv_type;
-	if (result->size.depth == 1)      { cv_type = CV_8UC1; }
-	else if (result->size.depth == 3) { cv_type = CV_8UC3; }
-	else if (result->size.depth == 4) { cv_type = CV_8UC4; }
-	else { return; } // unknown depth
+	cv::Mat merged(cv::Size(result->size.width * 2, result->size.height), result->size.CvType());
 
-	cv::Mat merged(cv::Size(result->size.width * 2, result->size.height), cv_type);
-
-	cv::Mat leftSrc(cv::Size(result->size.width, result->size.height), cv_type, result->buffer_left.get());
+	cv::Mat leftSrc(cv::Size(result->size.width, result->size.height), result->size.CvType(), result->buffer_left.get());
 	cv::Mat left(merged, cv::Rect(cv::Point(0, 0), cv::Size(result->size.width, result->size.height)));
 	leftSrc.copyTo(left);
 
-	cv::Mat rightSrc(cv::Size(result->size.width, result->size.height), cv_type, result->buffer_right.get());
+	cv::Mat rightSrc(cv::Size(result->size.width, result->size.height), result->size.CvType(), result->buffer_right.get());
 	cv::Mat right(merged, cv::Rect(cv::Point(result->size.width, 0), cv::Size(result->size.width, result->size.height)));
 	rightSrc.copyTo(right);
 
