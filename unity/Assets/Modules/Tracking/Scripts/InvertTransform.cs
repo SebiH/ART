b54c0381bdd598c4fcd2a81ac98eb83bbee1bb5d
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2016-09-01 15:48:42 +0200

    Add&use script to invert given transform
    Needed to turn off VR headtracking again ...

---
+++ unity/Assets/Modules/Tracking/Scripts/InvertTransform.cs
@@ -0,0 +1,21 @@
+﻿using Assets.Modules.Core.Util;
+using UnityEngine;
+
+namespace Assets.Modules.Tracking
+{
+    public class InvertTransform : MonoBehaviour
+    {
+        public Transform OriginalTransform;
+
+        void Update()
+        {
+            var matrix = Matrix4x4.TRS(OriginalTransform.localPosition, OriginalTransform.localRotation, OriginalTransform.localScale);
+
+            matrix = matrix.inverse;
+
+            transform.localPosition = matrix.GetPosition();
+            transform.localRotation = matrix.GetRotation();
+            transform.localScale = matrix.GetScale();
+        }
+    }
+}
