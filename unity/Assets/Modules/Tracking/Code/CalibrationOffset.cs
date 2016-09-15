26cd0bae9dbabb45f61058907bd1e4390846cca4
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2016-09-15 20:43:31 +0200

    Save / load offsets

--- unity/Assets/Modules/Tracking/Code/CalibrationOffset.cs
+++ unity/Assets/Modules/Tracking/Code/CalibrationOffset.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using UnityEngine;
 
 namespace Assets.Modules.Tracking
@@ -10,5 +11,33 @@ namespace Assets.Modules.Tracking
 
         public static Quaternion OpenVrRotationOffset;
         public static Vector3 OptitrackToCameraOffset;
+
+
+        [Serializable]
+        private class Offsets
+        {
+            public Quaternion Rotation;
+            public Vector3 Position;
+        }
+
+        public static void SaveToFile(string filename)
+        {
+            var offsets = new Offsets
+            {
+                Rotation = OpenVrRotationOffset,
+                Position = OptitrackToCameraOffset
+            };
+
+            File.WriteAllText(filename, JsonUtility.ToJson(offsets));
+        }
+
+        public static void LoadFromFile(string filename)
+        {
+            var contents = File.ReadAllText(filename);
+            var offsets = JsonUtility.FromJson<Offsets>(contents);
+
+            OpenVrRotationOffset = offsets.Rotation;
+            OptitrackToCameraOffset = offsets.Position;
+        }
     }
 }
