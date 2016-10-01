cd2fd0a6cfde561862ee430c4bc011f7e6a2e21d
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2016-10-01 19:55:56 +0200

    Gather build files in gui output folder

--- tools/ImageProcessing/scripts/post_build.bat
+++ tools/ImageProcessing/scripts/post_build.bat
@@ -13,7 +13,7 @@ copy %output_dir%*.dll %unity_dir%
 copy %project_dir%thirdparty\dll\* %unity_dir%
 
 REM copy to GUI
-set imp_test_dir="%solution_dir%bin\gui\"
+set imp_test_dir="%solution_dir%build\bin\gui\"
 copy "%output_dir%*.dll" "%imp_test_dir%"
 copy "%project_dir%thirdparty\dll\*" "%imp_test_dir%"
 
