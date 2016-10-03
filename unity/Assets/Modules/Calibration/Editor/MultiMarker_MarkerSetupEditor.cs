027dea9bbbd81b0445bcda4cd7a864b9f7348c10
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2016-10-03 21:40:08 +0200

    Fix editor window not updating progress bar properly

--- unity/Assets/Modules/Calibration/Editor/MultiMarker_MarkerSetupEditor.cs
+++ unity/Assets/Modules/Calibration/Editor/MultiMarker_MarkerSetupEditor.cs
@@ -24,6 +24,7 @@ namespace Assets.Modules.Calibration
                 EditorGUI.ProgressBar(r, script.SetMarkerProgress, "Calibrating...");
                 GUILayout.Space(19);
                 EditorGUILayout.EndVertical();
+                Repaint();
             }
         }
     }
