a8035c2383eb0e50ec8d7fe94f057a7ff639590a
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2016-10-06 16:30:52 +0200

    Add WIP script to auto calibrate marker relation

---
+++ unity/Assets/Modules/Calibration/Scripts/MultiMarker_MarkerPreviewSetup.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using Assets.Modules.Tracking;
+using System.Collections.Generic;
+
+namespace Assets.Modules.Calibration
+{
+    public class MultiMarker_MarkerPreviewSetup : MonoBehaviour
+    {
+        public int AnchorId = -1;
+        public int Span1Id = -1;
+        public int Span2Id = -1;
+        public int ForwardId = -1;
+
+        public string OptitrackCalibratorName = "CalibrationHelper";
+
+        void OnEnable()
+        {
+            OptitrackListener.Instance.PosesReceived += OnOptitrackPoses;
+        }
+
+        void OnDisable()
+        {
+            OptitrackListener.Instance.PosesReceived -= OnOptitrackPoses;
+        }
+
+        private void OnOptitrackPoses(List<OptitrackPose> poses)
+        {
+            foreach (var pose in poses)
+            {
+                if (pose.RigidbodyName == OptitrackCalibratorName)
+                {
+                    CalibrateMarker(pose);
+                    enabled = false;
+                }
+            }
+        }
+
+        private void CalibrateMarker(OptitrackPose pose)
+        {
+            var markerSetup = GetComponent<MultiMarker_MarkerSetup>();
+            
+
+        }
+    }
+}
