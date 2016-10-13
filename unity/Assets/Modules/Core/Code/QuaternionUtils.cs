03c844d7037c2d5cb5eea23aeb95e2cf2dfb062a
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2016-10-13 12:29:08 +0200

    WIP

---
+++ unity/Assets/Modules/Core/Code/QuaternionUtils.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.Modules.Core.Code
+{
+    public static class QuaternionUtils
+    {
+        public static Quaternion Average(IEnumerable<Quaternion> rotationlist)
+        {
+            if (rotationlist.Count() == 0)
+            {
+                return Quaternion.identity;
+            }
+
+            float x = 0;
+            float y = 0;
+            float z = 0;
+            float w = 0;
+
+            foreach (var quat in rotationlist)
+            {
+                x += quat.x;
+                y += quat.y;
+                z += quat.z;
+                w += quat.w;
+            }
+
+            float k = 1.0f / Mathf.Sqrt(x * x + y * y + z * z + w * w);
+            return new Quaternion(x * k, y * k, z * k, w * k);
+        }
+    }
+}
