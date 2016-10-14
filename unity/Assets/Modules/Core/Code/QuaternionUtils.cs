4cacd8c3442c887565965fb303e3f62862b8997e
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2016-10-14 17:05:01 +0200

    Adjust namespace

--- unity/Assets/Modules/Core/Code/QuaternionUtils.cs
+++ unity/Assets/Modules/Core/Code/QuaternionUtils.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-namespace Assets.Modules.Core.Code
+namespace Assets.Modules.Core.Util
 {
     public static class QuaternionUtils
     {
