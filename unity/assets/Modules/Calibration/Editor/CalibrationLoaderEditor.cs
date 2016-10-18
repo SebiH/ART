d04a754f15500f52c28dfb7f44e3f4689bca826d
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2016-10-18 13:43:18 +0200

    Add script to load calibration offsets

---
+++ unity/assets/Modules/Calibration/Editor/CalibrationLoaderEditor.cs
@@ -0,0 +1,26 @@
+﻿using Assets.Modules.Tracking;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Modules.Calibration
+{
+    [CustomEditor(typeof(CalibrationLoader))]
+    class CalibrationLoaderEditor : Editor
+    {
+        private string _filename = "";
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            var script = target as CalibrationLoader;
+
+            GUILayout.Label("CalibrationOffsets filename");
+            _filename = GUILayout.TextField(_filename);
+
+            if (GUILayout.Button("Load Calibration Offsets") && Application.isPlaying)
+            {
+                CalibrationOffset.LoadFromFile(_filename);
+            }
+        }
+    }
+}
---
+++ unity/assets/Modules/Calibration/Scripts/CalibrationLoader.cs
@@ -0,0 +1,19 @@
+﻿using Assets.Modules.Tracking;
+using UnityEngine;
+
+namespace Assets.Modules.Calibration
+{
+    // See relevant Editor script
+    public class CalibrationLoader : MonoBehaviour
+    {
+        public bool LoadDefaultOnStartup = false;
+
+        void OnEnable()
+        {
+            if (LoadDefaultOnStartup)
+            {
+                CalibrationOffset.LoadFromFile("default_calib.json");
+            }
+        }
+    }
+}
