caff8f0a2aa6bb58f7412e1e1937c7e12d82212b
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2016-10-21 14:40:05 +0200

    Add new calibration approach based on autocalibration

---
+++ unity/Assets/Modules/Calibration/Editor/DisplayMarker_SetupEditor.cs
@@ -0,0 +1,48 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Modules.Calibration
+{
+    [CustomEditor(typeof(MultiMarker_MarkerSetup))]
+    public class DisplayMarker_SetupEditor : Editor
+    {
+        private static string _calibrateMarkerId = "";
+        private static string _filename = "";
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            var script = target as DisplayMarker_SetupDisplay;
+
+            if (script.CalibrationProgress < Mathf.Epsilon)
+            {
+                if (GUILayout.Button("Set Marker") && Application.isPlaying)
+                {
+                    int id = int.Parse(_calibrateMarkerId);
+                    script.StartCalibration();
+                }
+            }
+            else
+            {
+                Rect r = EditorGUILayout.BeginVertical();
+                EditorGUI.ProgressBar(r, script.CalibrationProgress, "Calibrating...");
+                GUILayout.Space(19);
+                EditorGUILayout.EndVertical();
+                Repaint();
+            }
+
+
+            //GUILayout.Label("CalibrationOffsets filename");
+            //_filename = GUILayout.TextField(_filename);
+            //if (GUILayout.Button("Dump Markers to file") && Application.isPlaying)
+            //{
+            //    script.SaveCalibratedMarkers(_filename);
+            //}
+
+            //if (GUILayout.Button("Load Markerdump") && Application.isPlaying)
+            //{
+            //    script.LoadCalibratedMarkers(_filename);
+            //}
+        }
+    }
+}
---
+++ unity/Assets/Modules/Calibration/Scripts/DisplayMarker_SetupDisplay.cs
@@ -0,0 +1,110 @@
+using Assets.Modules.Tracking;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Modules.Calibration
+{
+    public class DisplayMarker_SetupDisplay : MonoBehaviour
+    {
+        // Editor
+        public DisplayMarker_PerformCalibration.MarkerOffset.Corner CurrentCorner;
+        public string CalibratorName = "CalibrationHelper";
+        public int CalibratorMarkerIndex = 4;
+
+
+        // For other scripts
+        public struct DisplayCorner
+        {
+            public DisplayMarker_PerformCalibration.MarkerOffset.Corner Corner;
+            public Vector3 Position;
+        }
+
+        public List<DisplayCorner> CalibratedCorners { get; private set; }
+        public float CalibrationProgress { get; private set; }
+
+        private bool _hasCalibratorPosition = false;
+        private Vector3 _currentCalibratorPosition;
+
+        void OnEnable()
+        {
+            CalibratedCorners = new List<DisplayCorner>();
+            CalibrationProgress = 0;
+            OptitrackListener.Instance.PosesReceived += OnOptitrackPose;
+        }
+
+        void OnDisable()
+        {
+            OptitrackListener.Instance.PosesReceived -= OnOptitrackPose;
+        }
+
+        private void OnOptitrackPose(List<OptitrackPose> poses)
+        {
+            foreach (var pose in poses)
+            {
+                if (pose.RigidbodyName == CalibratorName)
+                {
+                    foreach (var marker in pose.Markers)
+                    {
+                        if (marker.Id == CalibratorMarkerIndex)
+                        {
+                            _currentCalibratorPosition = marker.Position;
+                            _hasCalibratorPosition = true;
+                        }
+                    }
+                }
+            }
+        }
+
+
+        public void StartCalibration()
+        {
+            if (_hasCalibratorPosition)
+            {
+                StartCoroutine(Calibrate());
+            }
+        }
+
+        public IEnumerator Calibrate()
+        {
+            var samples = 0;
+            var maxSamples = 100;
+            var totalPositions = Vector3.zero;
+
+            while (samples < maxSamples)
+            {
+                totalPositions += _currentCalibratorPosition;
+                ++samples;
+                yield return new WaitForSeconds(0.01f);
+            }
+
+            var avgPosition = totalPositions / maxSamples;
+
+            CalibratedCorners.Add(new DisplayCorner
+            {
+                Position = avgPosition,
+                Corner = CurrentCorner
+            });
+        }
+
+
+        void OnDrawGizmosSelected()
+        {
+            if (Application.isPlaying)
+            {
+                if (_hasCalibratorPosition)
+                {
+                    Gizmos.color = Color.green;
+                    Gizmos.DrawSphere(_currentCalibratorPosition, 0.02f);
+                }
+
+                foreach (var cc in CalibratedCorners)
+                {
+                    Gizmos.color = Color.magenta;
+                    Gizmos.DrawWireSphere(cc.Position, 0.1f);
+                }
+            }
+        }
+
+    }
+}
