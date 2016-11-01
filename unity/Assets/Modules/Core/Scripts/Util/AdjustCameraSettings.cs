f8747ea4b83bc0b9d96b054d22adb4a5492b7bd8
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2016-11-01 16:54:25 +0100

    Add range for easier configuration

--- unity/Assets/Modules/Core/Scripts/Util/AdjustCameraSettings.cs
+++ unity/Assets/Modules/Core/Scripts/Util/AdjustCameraSettings.cs
@@ -1,14 +1,15 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 /**
  *  Adjusts a few camera settings which are overridden by SteamVR (e.g. FoV)
  */
-public class AdjustCameraSettings : MonoBehaviour {
-
+public class AdjustCameraSettings : MonoBehaviour
+{
+    [Range(60, 120)]
     public int TargetFieldOfView;
 
-	void Update ()
+    void Update()
     {
         GetComponent<Camera>().fieldOfView = TargetFieldOfView;
         //Destroy(GetComponent<AdjustCameraSettings>());
