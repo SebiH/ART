e594e29287a3994255cc4e078ebb678a2ccca89e
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2016-11-01 17:05:15 +0100

    Remove/Rename outdated scripts

--- unity/Assets/Modules/Calibration/Scripts/SingleMarker_PerformCalibration.cs
+++ unity/Assets/Modules/Calibration/Scripts/SingleMarker_PerformCalibration.cs
@@ -10,7 +10,7 @@ using Valve.VR;
 
 namespace Assets.Modules.Calibration
 {
-    public class PerformCalibrationSingleMarker : MonoBehaviour
+    public class SingleMarker_PerformCalibration : MonoBehaviour
     {
         const float SteadyPosThreshold = 0.2f;
         const float SteadyAngleThreshold = 2f;
