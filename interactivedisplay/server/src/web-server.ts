5f93d381aa2df92acae9cf9ed361b3f146fa107a
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2016-11-10 19:30:28 +0100

    Simplify server, implement missing methods

--- interactivedisplay/server/src/web-server.ts
+++ interactivedisplay/server/src/web-server.ts
@@ -1,7 +1,6 @@
 import * as http from 'http';
 import * as path from 'path';
 import * as express from 'express';
-import * as bodyParser from 'body-parser';
 
 import { Util } from './util';
 
@@ -10,28 +9,9 @@ export class WebServer {
     private _server: http.Server;
 
     public Start(port: number): void {
-        var app = express();
+        let app = express();
         app.set('port', port);
         app.use(express.static(path.join(__dirname, '../../client/')));
-        app.use(bodyParser.json());
-
-        app.post('/click', (req, res) => {
-            // TODO: get rid of this terrible hack
-            var width = 1920; // width as defined in index.html
-            var height = 1080; // height as defined in index.html
-
-            var x = req.body.x / width;
-            var y = req.body.y / height;
-
-            // convert to proper format, aka. byte array of 2 floats
-            var floatBuffer = new Buffer(2 * 4);
-            floatBuffer.writeFloatLE(x, 0);
-            floatBuffer.writeFloatLE(y, 4);
-
-            // sendToUnity(floatBuffer);
-
-            res.end();
-        });
 
         // send clients to index page on 404
         app.use((req, res, next) => {
@@ -49,7 +29,7 @@ export class WebServer {
     }
 
     public Stop(): void {
-
+        this._server.close();
     }
 }
 
