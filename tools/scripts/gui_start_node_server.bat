37a7387a6c9182ccaa51ff7da76e6e72dc1c8af2
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2016-11-10 18:06:42 +0100

    Start node server only from GUI
    Client only needs to be compiled once outside of development, anyway

--- tools/scripts/gui_start_node_server.bat
+++ tools/scripts/gui_start_node_server.bat
@@ -2,8 +2,8 @@ setlocal EnableDelayedExpansion
 
 REM variables
 set current_dir=%~dp0
-set node_dir="%current_dir%..\..\interactivedisplay\"
+set node_dir="%current_dir%..\..\interactivedisplay\server"
 
 REM start server
 cd %node_dir%
-start.sh
+npm start
