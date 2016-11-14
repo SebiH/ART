cb9f8d277feb6b430d8a509572c473f248c2edb9
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2016-11-14 18:49:12 +0100

    Fix incorrect cm/m conversion

--- unity/Assets/Modules/InteractiveSurface/Code/DisplayUtility.cs
+++ unity/Assets/Modules/InteractiveSurface/Code/DisplayUtility.cs
@@ -4,18 +4,18 @@ namespace Assets.Modules.InteractiveSurface
 {
     public static class DisplayUtility
     {
-        public static float PixelToCmRatio = 1;
+        public static float PixelToCmRatio = 0.01f;
 
         public static float PixelToUnityCoord(int pixelLocation)
         {
             // pixel coords uses cm, unity uses m
-            return PixelToCmRatio * pixelLocation * 100;           
+            return PixelToCmRatio * pixelLocation / 100f;           
         }
 
         public static int UnityToPixelLocation(float unityCoord)
         {
             // pixel coords uses cm, unity uses m
-            return Mathf.RoundToInt((1 / PixelToCmRatio) * (unityCoord / 100f));
+            return Mathf.RoundToInt((1 / PixelToCmRatio) * unityCoord * 100f);
         }
     }
 }
