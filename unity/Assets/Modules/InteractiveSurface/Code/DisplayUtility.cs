265617fa0ff9303a49298d8482e4329acd733489
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2016-11-14 23:06:19 +0100

    Allow float conversions

--- unity/Assets/Modules/InteractiveSurface/Code/DisplayUtility.cs
+++ unity/Assets/Modules/InteractiveSurface/Code/DisplayUtility.cs
@@ -6,16 +6,16 @@ namespace Assets.Modules.InteractiveSurface
     {
         public static float PixelToCmRatio = 0.01f;
 
-        public static float PixelToUnityCoord(int pixelLocation)
+        public static float PixelToUnityCoord(float pixelLocation)
         {
             // pixel coords uses cm, unity uses m
             return PixelToCmRatio * pixelLocation / 100f;           
         }
 
-        public static int UnityToPixelLocation(float unityCoord)
+        public static float UnityToPixelLocation(float unityCoord)
         {
             // pixel coords uses cm, unity uses m
-            return Mathf.RoundToInt((1 / PixelToCmRatio) * unityCoord * 100f);
+            return (1 / PixelToCmRatio) * unityCoord * 100f;
         }
     }
 }
