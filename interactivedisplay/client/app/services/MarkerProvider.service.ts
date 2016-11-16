b68899a0f182eaa7e93d220c231311c3145fb21e
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2016-11-16 16:13:40 +0100

    Adjust markersize, count

--- interactivedisplay/client/app/services/MarkerProvider.service.ts
+++ interactivedisplay/client/app/services/MarkerProvider.service.ts
@@ -25,21 +25,22 @@ export class MarkerProvider {
             marker.offPropertyChanged((marker) => this.syncMarker(marker));
         }
 
-        let markerSize = 200;
+        let markerSize = 400;
         let borderSize = 50;
         let currentId = 0;
-        let markerCount = 5;
+        let markerCountHor = 3;
+        let markerCountVer = 2;
 
         let width = window.innerWidth - borderSize * 2;
         let height = window.innerHeight - borderSize * 2;
 
         // distribute markers on top
-        for (let i = 0; i < markerCount; i++) {
-            for (let j = 0; j < markerCount; j++) {
+        for (let i = 0; i < markerCountHor; i++) {
+            for (let j = 0; j < markerCountVer; j++) {
                 let marker = new Marker();
                 marker.id = currentId++;
-                marker.posX = Math.max(i / (markerCount - 1) * width - markerSize, 0) + borderSize;
-                marker.posY = Math.max(j / (markerCount - 1) * height - markerSize, 0) + borderSize;
+                marker.posX = Math.max(i / (markerCountHor - 1) * width - markerSize, 0) + borderSize;
+                marker.posY = Math.max(j / (markerCountVer - 1) * height - markerSize, 0) + borderSize;
                 marker.size = markerSize;
                 marker.onPropertyChanged((marker) => this.syncMarker(marker));
                 this.markers.push(marker);
