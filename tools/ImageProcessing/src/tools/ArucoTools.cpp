06c0b45576cadf05ca4f1fef40b3ccf9a3238df1
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2016-11-16 22:04:16 +0100

    Add generator for aruco markermaps
    This time with the correct files

--- tools/ImageProcessing/src/tools/ArucoTools.cpp
+++ tools/ImageProcessing/src/tools/ArucoTools.cpp
@@ -23,3 +23,55 @@ std::vector<std::string> ArucoTools::GetDictionaries()
 {
 	return aruco::Dictionary::getDicTypes();
 }
+
+namespace Params
+{
+	static const auto SizeX = "sizeX";
+	static const auto SizeY = "sizeY";
+	static const auto MarkerIds = "markerIds";
+	static const auto ImageFilename = "imgFilename";
+	static const auto ConfigFilename = "configFilename";
+	static const auto DictionaryName = "dictionaryName";
+
+	static const auto PixelSize = "pixelSize";
+	static const auto InterMarkerDistance = "interMarkerDistance";
+}
+
+void ArucoTools::GenerateMarkerMap(nlohmann::json config)
+{
+
+	int size_x = config[Params::SizeX].get<int>();
+	int size_y = config[Params::SizeY].get<int>();
+
+	auto img_filename = config[Params::ImageFilename].get<std::string>();
+	auto config_filename = config[Params::ConfigFilename].get<std::string>();
+
+	auto dict = aruco::Dictionary::loadPredefined(config[Params::DictionaryName].get<std::string>());
+	const int type_markermap = 0; // 1 for chessboard
+	
+	int pixel_size = 500;
+	if (config.count(Params::PixelSize))
+	{
+		pixel_size = config[Params::PixelSize].get<int>();
+	}
+
+	float inter_marker_distance = 0.2f;
+	if (config.count(Params::InterMarkerDistance))
+	{
+		inter_marker_distance = config[Params::InterMarkerDistance].get<float>();
+	}
+
+	if ((inter_marker_distance > 1.f) || (inter_marker_distance < 0.f))
+	{
+		throw std::exception("InterMarkerDistance has to be between 0 and 1");
+	}
+
+	auto marker_ids = config.get[Params::MarkerIds].get<std::vector<int>>();
+	aruco::MarkerMap markermap = dict.createMarkerMap(cv::Size(size_x, size_y), pixel_size, pixel_size * inter_marker_distance, marker_ids, type_markermap == 1);
+	//create a printable image to save
+	cv::Mat img_markermap = markermap.getImage();
+
+	//save
+	markermap.saveToFile(config_filename);
+	cv::imwrite(img_filename, img_markermap);
+}
--- tools/ImageProcessing/src/tools/ArucoTools.h
+++ tools/ImageProcessing/src/tools/ArucoTools.h
@@ -4,6 +4,7 @@
 #include <vector>
 
 #include <opencv2/opencv.hpp>
+#include <json/json.hpp>
 
 namespace ImageProcessing
 {
@@ -12,5 +13,21 @@ namespace ImageProcessing
 	public:
 		static std::vector<cv::Mat> GenerateMarkers(std::string dictionary_name, int pixel_size);
 		static std::vector<std::string> GetDictionaries();
+		
+		/**
+		 *	int sizeX, int sizeY, std::string imgFilename, std::string configFilename
+		 *	{
+		 *		sizeX: number,
+		 *		sizeY: number,
+		 *		markerIds: number[],
+		 *		imgFilename: string,
+		 *		configFilename: string,
+		 *		dictionaryName: string,
+		 *		
+		 *		pixelSize: number?,
+		 *		interMarkerDistance: number(0,1)?
+		 *  }
+		 */
+		static void GenerateMarkerMap(nlohmann::json config);
 	};
 }
