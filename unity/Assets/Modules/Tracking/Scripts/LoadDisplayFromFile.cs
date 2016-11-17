67da319ea8f52cbe4d3129eadfcca4eeff6dd0e6
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2016-11-17 15:16:41 +0100

    Add script to automatically load FixedDisplays from file

---
+++ unity/Assets/Modules/Tracking/Scripts/LoadDisplayFromFile.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.Modules.Tracking.Scripts
+{
+    public class LoadDisplayFromFile : MonoBehaviour
+    {
+        public string StartupFilename = "default_displays.json";
+
+        void OnEnable()
+        {
+            FixedDisplays.LoadFromFile(StartupFilename);
+        }
+    }
+}
