f1ee581c45a3ce0b8525a0d93fe2ee3a4ba58f54
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2016-11-21 22:41:02 +0100

    Load only currently visible data, to improve perfomance with big data sets

---
+++ unity/Assets/Modules/Graph/Scripts/DataLoader/DataLoader.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace Assets.Modules.Graph
+{
+    public abstract class DataLoader : MonoBehaviour
+    {
+    }
+}
---
+++ unity/Assets/Modules/Graph/Scripts/DataLoader/DynamicDataLoader.cs
@@ -0,0 +1,125 @@
+using Assets.Modules.Heatmap;
+using UnityEngine;
+
+namespace Assets.Modules.Graph
+{
+    public class DynamicDataLoader : DataLoader
+    {
+        public HeatmapModelDynamic Heatmap;
+
+        private GameObject[,] _generatedObjects;
+
+        void OnEnable()
+        {
+            var data = Heatmap.Provider.GetData();
+            var rows = data.GetLength(0);
+            var cols = data.GetLength(1);
+
+            _generatedObjects = new GameObject[rows, cols];
+
+            // TODO: start up by placing first datapoint in middle of hitbox
+            var hr = 0;
+            var hc = 0;
+
+            var point = Heatmap.GenerateDataPoint(hr, hc);
+            _generatedObjects[hr, hc] = point;
+            LoadRegionAround(point.GetComponent<DataPoint>());
+        }
+
+        void OnDisable()
+        {
+
+        }
+
+        void OnTriggerEnter(Collider col)
+        {
+            var dataPoint = col.GetComponent<DataPoint>();
+
+            if (dataPoint != null)
+            {
+                LoadRegionAround(dataPoint);
+            }
+        }
+
+        void OnTriggerExit(Collider col)
+        {
+            var dataPoint = col.GetComponent<DataPoint>();
+
+            if (dataPoint != null)
+            {
+                UnloadRegionAround(dataPoint);
+            }
+        }
+
+        // radius around which points are (un)loaded
+        const int LOAD_REGION_RADIUS = 1;
+
+        private void LoadRegionAround(DataPoint p)
+        {
+            p.GetComponent<Renderer>().enabled = true;
+
+            var maxRows = _generatedObjects.GetLength(0) - 1;
+            var maxCols = _generatedObjects.GetLength(1) - 1;
+
+            var startRow = Mathf.Max(p.RowIndex - LOAD_REGION_RADIUS, 0);
+            var endRow = Mathf.Min(p.RowIndex + LOAD_REGION_RADIUS, maxRows);
+
+            var startCol = Mathf.Max(p.ColumnIndex - LOAD_REGION_RADIUS, 0);
+            var endCol = Mathf.Min(p.ColumnIndex + LOAD_REGION_RADIUS, maxCols);
+
+            for (int row = startRow; row <= endRow; row++)
+            {
+                for (int col = startCol; col <= endCol; col++)
+                {
+                    if (row == p.RowIndex && col == p.ColumnIndex)
+                    {
+                        continue;
+                    }
+
+                    if (_generatedObjects[row, col] == null)
+                    {
+                        var newPoint = Heatmap.GenerateDataPoint(row, col);
+                        newPoint.GetComponent<Renderer>().enabled = false;
+                        _generatedObjects[row, col] = newPoint;
+                    }
+                }
+            }
+        }
+
+        private void UnloadRegionAround(DataPoint p)
+        {
+            p.GetComponent<Renderer>().enabled = false;
+
+            var maxRows = _generatedObjects.GetLength(0) - 1;
+            var maxCols = _generatedObjects.GetLength(1) - 1;
+
+            // Note: the surrounding datapoints should always be active, so that panning
+            // always loads data if user pans model completely outside
+            // -> max(1, ...); min(max - 1, ...) instead of 0
+            var startRow = Mathf.Max(p.RowIndex - LOAD_REGION_RADIUS, 1);
+            var endRow = Mathf.Min(p.RowIndex + LOAD_REGION_RADIUS, maxRows - 1);
+
+            var startCol = Mathf.Max(p.ColumnIndex - LOAD_REGION_RADIUS, 1);
+            var endCol = Mathf.Min(p.ColumnIndex + LOAD_REGION_RADIUS, maxCols - 1);
+
+            for (int row = startRow; row <= endRow; row++)
+            {
+                for (int col = startCol; col <= endCol; col++)
+                {
+                    if (row == p.RowIndex && col == p.ColumnIndex)
+                    {
+                        continue;
+                    }
+
+                    var oldPoint = _generatedObjects[row, col];
+
+                    if (oldPoint != null && !(oldPoint.GetComponent<Renderer>().isVisible))
+                    {
+                        _generatedObjects[row, col] = null;
+                        Destroy(oldPoint);
+                    }
+                }
+            }
+        }
+    }
+}
