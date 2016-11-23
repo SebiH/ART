829edd51cdfdf429ae64f6a87eaee2a4679ed899
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2016-11-23 11:23:12 +0100

    Add cube for debugging aruco calibration state

---
+++ unity/Assets/Modules/Calibration/Scripts/Debug_PerpetualTrackNearestMarker.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using Assets.Modules.Tracking;
+
+namespace Assets.Modules.Calibration
+{
+    public class Debug_PerpetualTrackNearestMarker : MonoBehaviour
+    {
+        public Perpetual_Calibration CalibrationScript;
+        public ArucoTrackedObject TrackingScript;
+        
+        void Update()
+        {
+
+            if (CalibrationScript.UseMultiMarker)
+            {
+                if (CalibrationScript.__nearestMarkerIds.Count > 0)
+                {
+                    TrackingScript.TrackedId = CalibrationScript.__nearestMarkerIds[0];
+                }
+            }
+            else
+            {
+                TrackingScript.TrackedId = CalibrationScript.__nearestMarkerId;
+            }
+
+            transform.localScale = Vector3.one * ArucoListener.Instance.MarkerSizeInMeter;
+        }
+    }
+}
