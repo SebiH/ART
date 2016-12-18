ca7276ade9ce5de6402cf82dfd8dcae7577378e0
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2016-12-18 20:28:05 +0100

    Fix wrong colour range ([0,1] instead of [0,255])

--- unity/Assets/Modules/Core/Code/Theme.cs
+++ unity/Assets/Modules/Core/Code/Theme.cs
@@ -7,25 +7,25 @@ namespace Assets.Modules.Core
         private static Color[] _themeColors =
         {
             // Material Theme colours - see https://material.io/guidelines/style/color.html
-            new Color(244, 67, 54), // red
-            new Color(233, 30, 99), // pink
-            new Color(156, 39, 176), // purple
-            new Color(103, 58, 183), // deep purple
-            new Color(63, 81, 181), // indigo
-            new Color(33, 150, 243), // blue
-            new Color(3, 169, 244), // light blue
-            new Color(0, 188, 212), // cyan
-            new Color(0, 150, 136), // teal
-            new Color(76, 175, 80), // green
-            new Color(139, 195, 74), // light green
-            new Color(205, 220, 57), // lime
-            new Color(255, 235, 59), // yellow
-            new Color(255, 193, 7), // amber
-            new Color(255, 152, 0), // orange
-            new Color(255, 87, 34), // deep orange
-            new Color(121, 85, 72), // brown
-            new Color(158, 158, 158), // grey
-            new Color(96, 125, 139) // blue grey
+            new Color(244f/255f, 67f/255f, 54f/255f), // red
+            new Color(233f/255f, 30f/255f, 99f/255f), // pink
+            new Color(156f/255f, 39f/255f, 176f/255f), // purple
+            new Color(103f/255f, 58f/255f, 183f/255f), // deep purple
+            new Color(63f/255f, 81f/255f, 181f/255f), // indigo
+            new Color(33f/255f, 150f/255f, 243f/255f), // blue
+            new Color(3f/255f, 169f/255f, 244f/255f), // light blue
+            new Color(0f/255f, 188f/255f, 212f/255f), // cyan
+            new Color(0f/255f, 150f/255f, 136f/255f), // teal
+            new Color(76f/255f, 175f/255f, 80f/255f), // green
+            new Color(139f/255f, 195f/255f, 74f/255f), // light green
+            new Color(205f/255f, 220f/255f, 57f/255f), // lime
+            new Color(255f/255f, 235f/255f, 59f/255f), // yellow
+            new Color(255f/255f, 193f/255f, 7f/255f), // amber
+            new Color(255f/255f, 152f/255f, 0f/255f), // orange
+            new Color(255f/255f, 87f/255f, 34f/255f), // deep orange
+            new Color(121f/255f, 85f/255f, 72f/255f), // brown
+            new Color(158f/255f, 158f/255f, 158f/255f), // grey
+            new Color(96f/255f, 125f/255f, 139f/255f) // blue grey
         };
 
         public static Color GetColor(int index)
