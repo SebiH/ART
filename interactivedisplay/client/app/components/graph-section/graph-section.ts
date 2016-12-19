635c451061078d42671a006e78d45a38d5fb838a
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2016-12-19 16:11:46 +0100

    Send graph section position to unity

--- interactivedisplay/client/app/components/graph-section/graph-section.ts
+++ interactivedisplay/client/app/components/graph-section/graph-section.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit, OnDestroy, ElementRef } from '@angular/core';
-import { Marker, Graph } from '../../models/index';
-import { MarkerProvider } from '../../services/index';
+import { Observable, Subscription } from 'rxjs/Rx';
+import { Marker, Graph, Point } from '../../models/index';
+import { MarkerProvider, SocketIO } from '../../services/index';
 
 @Component({
   selector: 'graph-section',
@@ -9,16 +10,54 @@ import { MarkerProvider } from '../../services/index';
 })
 export class GraphSectionComponent implements OnInit, OnDestroy {
 
-    @Input() private graph: Graph;
-    private marker: Marker;
+  @Input() private graph: Graph;
+  private marker: Marker;
+  private prevPosition: Point;
+  private timerSubscription: Subscription;
+  private socketioListener;
 
-    constructor (private markerProvider: MarkerProvider) {}
+  constructor (
+    private markerProvider: MarkerProvider,
+    private socketio: SocketIO,
+    private elementRef: ElementRef
+    ) {}
 
-    ngOnInit() {
-      this.marker = this.markerProvider.createMarker();
+  ngOnInit() {
+    this.marker = this.markerProvider.createMarker();
+
+    let timer = Observable.timer(0, 100);
+    this.timerSubscription = timer.subscribe(this.checkForChanges.bind(this));
+
+    this.socketioListener =  () => { this.sendPosition(this.getSectionPosition()); };
+    this.socketio.on('get-plane', this.socketioListener);
+  }
+
+  ngOnDestroy() {
+    this.markerProvider.destroyMarker(this.marker);
+    this.timerSubscription.unsubscribe();
+    this.socketio.off('get-plane', this.socketioListener);
+  }
+
+  private checkForChanges() {
+    let currentPosition = this.getSectionPosition();
+
+    if (this.prevPosition == undefined || !this.prevPosition.equalTo(currentPosition)) {
+      this.sendPosition(currentPosition);
+      this.prevPosition = currentPosition;
     }
+  }
 
-    ngOnDestroy() {
-      this.markerProvider.destroyMarker(this.marker);
-    }
+  private getSectionPosition(): Point {
+    let element = <HTMLElement>this.elementRef.nativeElement;
+    let pos = element.getBoundingClientRect();
+
+    return new Point(pos.left, pos.top);
+  }
+
+  private sendPosition(pos: Point) {
+    this.socketio.sendMessage('plane-position', {
+      id: this.graph.id,
+      pos: pos.x
+    });
+  }
 }
