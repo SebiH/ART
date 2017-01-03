e8859a6efbcd364e65a4a744fc3559c074b189a3
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2017-01-03 23:59:48 +0100

    Add d3 dependency

--- interactivedisplay/client/systemjs.config.js
+++ interactivedisplay/client/systemjs.config.js
@@ -18,9 +18,10 @@
       '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
       // other libraries
-      'rxjs':                      'npm:rxjs',
+      'rxjs': 'npm:rxjs',
       'socket.io-client': '/socket.io/socket.io.js',
-      'lodash': 'npm:lodash/lodash.min.js'
+      'lodash': 'npm:lodash/lodash.min.js',
+      'd3': 'npm:d3/build/d3.js'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
