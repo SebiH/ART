a24e23e6d5a1cea0982eaeff87547f9d4a89bcf8
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2017-01-04 17:16:14 +0100

    Graph data overhaul

--- interactivedisplay/client/app/models/graph.ts
+++ interactivedisplay/client/app/models/graph.ts
@@ -1,3 +1,5 @@
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 
 export class Graph {
     public id: number = -1;
@@ -8,11 +10,52 @@ export class Graph {
     public selectedDataIndices: number[] = [];
     public isSelected: boolean = false;
 
+    public absolutePos: number = 0;
+    public listIndex: number = 0;
+    public width: number = 100;
+
+
+    private dataSubscription: Subject<any> = new Subject<any>();
+    public get onDataUpdate() : Observable<any> {
+        return this.dataSubscription.asObservable();
+    }
+
+    public updateData() {
+        this.dataSubscription.next({
+            id: this.id,
+            dimX: this.dimX,
+            dimY: this.dimY,
+            selectedData: this.selectedDataIndices,
+            isSelected: this.isSelected
+        });
+    }
+
+
+    public positionSubscription: Subject<any> = new Subject<any>();
+    public get onPositionUpdate(): Observable<any> {
+        return this.positionSubscription.asObservable();
+    }
+
+    public updatePosition() {
+        this.positionSubscription.next({
+            id: this.id,
+            pos: this.absolutePos,
+            index: this.listIndex
+        });
+    } 
+
+
     public toJson(): any {
         return {
             id: this.id,
+            
             dimX: this.dimX,
-            dimY: this.dimY
-        };
+            dimY: this.dimY,
+            selectedData: this.selectedDataIndices,
+            isSelected: this.isSelected,
+
+            pos: this.absolutePos,
+            index: this.listIndex
+        }
     }
 }
--- interactivedisplay/client/app/models/index.ts
+++ interactivedisplay/client/app/models/index.ts
@@ -2,4 +2,3 @@ export * from './marker';
 export * from './map';
 export * from './point';
 export * from './graph';
-export * from './graph-position';
