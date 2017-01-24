a866021cd942556001badf379f6b33c9cb0411bf
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2017-01-24 22:46:11 +0100

    Track estimated camera position based on ar marker

--- unity/Assets/Modules/Calibration/Scripts/ArMarker.cs
+++ unity/Assets/Modules/Calibration/Scripts/ArMarker.cs
@@ -1,28 +1,54 @@
+using Assets.Modules.Tracking;
 using UnityEngine;
 
 namespace Assets.Modules.Calibration
 {
     public class ArMarker : MonoBehaviour
     {
-        public int Id = -1;
-        public Vector2 Position = Vector2.zero;
-        public float Size = 0.25f;
+        public int Id { get; set; }
+        public float LastChangeTime { get; private set; }
+
+        private Vector2 _position = Vector2.zero;
+        public Vector2 Position
+        {
+            get { return _position; }
+            set
+            {
+                _position = value;
+                transform.localPosition = new Vector3(value.x, 0, value.y);
+                LastChangeTime = Time.unscaledTime;
+                HasDetectedCamera = false;
+            }
+        }
+
+        private float _size = 0.25f;
+        public float Size
+        {
+            get { return _size; }
+            set
+            {
+                _size = value;
+                LastChangeTime = Time.unscaledTime;
+            }
+        }
+
+        public bool HasDetectedCamera { get; private set; }
+        public Vector3 DetectedCameraPosition { get; private set; }
+        public Quaternion DetectedCameraRotation { get; private set; }
+        public float CameraDetectionTime { get; private set; }
 
         void OnEnable()
         {
             Register();
             ArMarkers.Add(this);
+            ArucoListener.Instance.NewPoseDetected += OnArPose;
         }
 
         void OnDisable()
         {
             Deregister();
             ArMarkers.Remove(this);
-        }
-
-        void Update()
-        {
-            transform.localPosition = new Vector3(Position.x, 0, Position.y);
+            ArucoListener.Instance.NewPoseDetected -= OnArPose;
         }
 
         private void Register()
@@ -34,7 +60,28 @@ namespace Assets.Modules.Calibration
         {
             // TODO
         }
-        
+
+
+        void Update()
+        {
+            if (HasDetectedCamera && (Time.unscaledTime - CameraDetectionTime) > 0.5f)
+            {
+                HasDetectedCamera = false;
+            }
+        }
+
+        private void OnArPose(MarkerPose pose)
+        {
+            Matrix4x4 marker = Matrix4x4.TRS(pose.Position, pose.Rotation, Vector3.one);
+            Matrix4x4 cam = marker.inverse;
+
+            HasDetectedCamera = true;
+            DetectedCameraPosition = transform.position + cam.GetPosition();
+            DetectedCameraRotation = transform.rotation * cam.GetRotation();
+            CameraDetectionTime = Time.unscaledTime;
+        }
+
+
         void OnDrawGizmos()
         {
             // Draw center
@@ -52,6 +99,29 @@ namespace Assets.Modules.Calibration
             Gizmos.DrawLine(bl, br);
             Gizmos.DrawLine(br, tr);
             Gizmos.DrawLine(tr, tl);
+
+            if (HasDetectedCamera)
+            {
+                // draw detected camera pose
+                Gizmos.DrawWireSphere(DetectedCameraPosition, 0.01f);
+
+                // draw rotation of camera
+                Gizmos.color = Color.blue;
+                var camForward = DetectedCameraRotation * Vector3.forward;
+                Gizmos.DrawLine(DetectedCameraPosition, DetectedCameraPosition + camForward * 0.1f);
+
+                Gizmos.color = Color.green;
+                var camUp = DetectedCameraRotation * Vector3.up;
+                Gizmos.DrawLine(DetectedCameraPosition, DetectedCameraPosition + camUp * 0.01f);
+
+                Gizmos.color = Color.red;
+                var camRight = DetectedCameraRotation * Vector3.right;
+                Gizmos.DrawLine(DetectedCameraPosition, DetectedCameraPosition + camRight * 0.01f);
+
+                // draw connection to marker for clarity
+                Gizmos.color = Color.gray;
+                Gizmos.DrawLine(DetectedCameraPosition, transform.position);
+            }
         }
     }
 }
