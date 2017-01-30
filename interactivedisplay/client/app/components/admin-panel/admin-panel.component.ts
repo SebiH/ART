4ca5a76f26ee51101d7c61251c97fb2db3f1398c
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2017-01-30 21:15:39 +0100

    Organize admin panels in tabs

--- interactivedisplay/client/app/components/admin-panel/admin-panel.component.ts
+++ interactivedisplay/client/app/components/admin-panel/admin-panel.component.ts
@@ -1,18 +1,14 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'admin-panel',
     templateUrl: './app/components/admin-panel/admin-panel.html',
     styleUrls: ['./app/components/admin-panel/admin-panel.css']
 })
-export class AdminPanelComponent implements OnInit, OnDestroy {
+export class AdminPanelComponent {
 
     private width: number = window.innerWidth * 0.8;
-
-    ngOnInit() {
-    }
-
-    ngOnDestroy() {
-
-    }
+    private tabs: string[] = [ 'Stability', 'Actions', 'Camera'];
+    private activeTab = this.tabs[0];
 }
+
