a79bb01e9ed7d4e402cbc20fb5e7e0dc0bea5617
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2017-02-04 00:44:52 +0100

    Move some chart properties to CSS again, disable d3 selection transitions
    (in favour of limited css transitions)

--- interactivedisplay/client/app/components/scatter-plot/chart-data.ts
+++ interactivedisplay/client/app/components/scatter-plot/chart-data.ts
@@ -31,14 +31,14 @@ export class ChartData {
                 .attr('cx', d => d[0])
                 .attr('cy', d => d[1])
                 .attr('r', 5)
-                .attr('fill', 'black');
+                .attr('class', 'scatter-point');
     }
 
     private animateValues(data: number[][]): void {
         this.chartRoot.selectAll('circle')
             .data(data)
             .transition()
-            .duration(200)
+            .duration(500)
             .ease(d3.easeLinear)
             .attr('cx', d => d[0])
             .attr('cy', d => d[1]);
@@ -47,43 +47,25 @@ export class ChartData {
 
     public highlight(selectedIds: number[], filteredIds: number[], useFilter: boolean): void {
         this.chartRoot.selectAll('circle')
-            // .attr('class', (d, i) => {
-            //     let cssClass = 'data';
-            //     if (selectedIds.indexOf(i) > -1) {
-            //         cssClass += ' selected';
-            //     }
-            //     if (useFilter && filteredIds.indexOf(i) < 0) {
-            //         cssClass += ' filtered';
-            //     }
-
-            //     return cssClass;
-            // });
-            // .transition()
-            // .duration(100)
-            // .ease(d3.easeLinear)
             .each(function(d, i) {
                 let isSelected = (selectedIds.indexOf(i) > -1);
                 let isFiltered = (useFilter && filteredIds.indexOf(i) < 0);
+                let cssClass = 'scatter-point';
 
                 let radius = 5;
-                let fill = 'black';
                 if (isFiltered && isSelected) {
-                    fill = 'red';
                     radius = 5;
                 } else if (isSelected) {
                     radius = 7;
-                    fill = 'green';
+                    cssClass += ' selected';
                 } else if (isFiltered) {
                     radius = 3;
-                    fill = 'gray';
+                    cssClass += ' filtered';
                 }
 
                 d3.select(this)
-                    .transition()
-                    .duration(200)
-                    .ease(d3.easeLinear)
-                    .attr('r', radius)
-                    .attr('fill', fill);
+                    .attr('class', cssClass)
+                    .attr('r', radius);
             });
     }
 }
