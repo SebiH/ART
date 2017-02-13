e750b17a2934ca4c9c35707a258b3c3520bf9301
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2017-02-13 21:10:58 +0100

    Use monochromatic (blue) colours for parallel coordinate lines

--- unity/Assets/Modules/Core/Code/Theme.cs
+++ unity/Assets/Modules/Core/Code/Theme.cs
@@ -4,28 +4,67 @@ namespace Assets.Modules.Core
 {
     public static class Theme
     {
+        //private static Color[] _themeColors =
+        //{
+        //    // Material Theme colours - see https://material.io/guidelines/style/color.html
+        //    new Color(244f/255f, 67f/255f, 54f/255f), // red
+        //    new Color(233f/255f, 30f/255f, 99f/255f), // pink
+        //    new Color(156f/255f, 39f/255f, 176f/255f), // purple
+        //    new Color(103f/255f, 58f/255f, 183f/255f), // deep purple
+        //    new Color(63f/255f, 81f/255f, 181f/255f), // indigo
+        //    new Color(33f/255f, 150f/255f, 243f/255f), // blue
+        //    new Color(3f/255f, 169f/255f, 244f/255f), // light blue
+        //    new Color(0f/255f, 188f/255f, 212f/255f), // cyan
+        //    new Color(0f/255f, 150f/255f, 136f/255f), // teal
+        //    new Color(76f/255f, 175f/255f, 80f/255f), // green
+        //    new Color(139f/255f, 195f/255f, 74f/255f), // light green
+        //    new Color(205f/255f, 220f/255f, 57f/255f), // lime
+        //    new Color(255f/255f, 235f/255f, 59f/255f), // yellow
+        //    new Color(255f/255f, 193f/255f, 7f/255f), // amber
+        //    new Color(255f/255f, 152f/255f, 0f/255f), // orange
+        //    new Color(255f/255f, 87f/255f, 34f/255f), // deep orange
+        //    new Color(121f/255f, 85f/255f, 72f/255f), // brown
+        //    new Color(158f/255f, 158f/255f, 158f/255f), // grey
+        //    new Color(96f/255f, 125f/255f, 139f/255f) // blue grey
+        //};
+
         private static Color[] _themeColors =
         {
-            // Material Theme colours - see https://material.io/guidelines/style/color.html
-            new Color(244f/255f, 67f/255f, 54f/255f), // red
-            new Color(233f/255f, 30f/255f, 99f/255f), // pink
-            new Color(156f/255f, 39f/255f, 176f/255f), // purple
-            new Color(103f/255f, 58f/255f, 183f/255f), // deep purple
-            new Color(63f/255f, 81f/255f, 181f/255f), // indigo
-            new Color(33f/255f, 150f/255f, 243f/255f), // blue
-            new Color(3f/255f, 169f/255f, 244f/255f), // light blue
-            new Color(0f/255f, 188f/255f, 212f/255f), // cyan
-            new Color(0f/255f, 150f/255f, 136f/255f), // teal
-            new Color(76f/255f, 175f/255f, 80f/255f), // green
-            new Color(139f/255f, 195f/255f, 74f/255f), // light green
-            new Color(205f/255f, 220f/255f, 57f/255f), // lime
-            new Color(255f/255f, 235f/255f, 59f/255f), // yellow
-            new Color(255f/255f, 193f/255f, 7f/255f), // amber
-            new Color(255f/255f, 152f/255f, 0f/255f), // orange
-            new Color(255f/255f, 87f/255f, 34f/255f), // deep orange
-            new Color(121f/255f, 85f/255f, 72f/255f), // brown
-            new Color(158f/255f, 158f/255f, 158f/255f), // grey
-            new Color(96f/255f, 125f/255f, 139f/255f) // blue grey
+            //// purples
+            //new Color(186f/255f, 104f/255f, 200f/255f), // 300
+            //new Color(171f/255f, 71f/255f, 188f/255f), // 400 
+            //new Color(156f/255f, 39f/255f, 176f/255f), // 500
+            //new Color(142f/255f, 36f/255f, 170f/255f), // 600
+            //new Color(123f/255f, 31f/255f, 162f/255f), // 700
+
+            //// deep purples
+            //new Color(149f/255f, 117f/255f, 205f/255f), // 300
+            //new Color(126f/255f, 87f/255f, 194f/255f), // 400
+            //new Color(103f/255f, 58f/255f, 183f/255f), // 500
+            //new Color(94f/255f, 53f/255f, 177f/255f), // 600
+            //new Color(81f/255f, 45f/255f, 168f/255f), // 700
+
+            // indigo
+            new Color(121f/255f, 134f/255f, 203f/255f), // 300
+            new Color(92f/255f, 107f/255f, 192f/255f), // 400
+            new Color(63f/255f, 81f/255f, 181f/255f), // 500
+            new Color(57f/255f, 73f/255f, 171f/255f), // 600
+            new Color(48f/255f, 63f/255f, 159f/255f), // 700
+
+            // blue
+            new Color(100f/255f, 181f/255f, 246f/255f), // 300
+            new Color(66f/255f, 165f/255f, 245f/255f), // 400
+            new Color(33f/255f, 150f/255f, 243f/255f), // 500
+            new Color(30f/255f, 136f/255f, 229f/255f), // 600
+            new Color(25f/255f, 118f/255f, 210f/255f), // 700
+
+            // light blue
+            new Color(79f/255f, 195f/255f, 247f/255f), // 300
+            new Color(41f/255f, 182f/255f, 246f/255f), // 400
+            new Color(3f/255f, 169f/255f, 244f/255f), // 500
+            new Color(3f/255f, 155f/255f, 229f/255f), // 600
+            new Color(2f/255f, 136f/255f, 209f/255f), // 700
+
         };
 
         public static Color GetColor(int index)
