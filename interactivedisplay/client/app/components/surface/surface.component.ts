7b891ba55863e21b73da718adb90f79919676725
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2017-02-18 20:58:49 +0100

    Disable back navigation
    For #73 but needs further testing

--- interactivedisplay/client/app/components/surface/surface.component.ts
+++ interactivedisplay/client/app/components/surface/surface.component.ts
@@ -10,10 +10,30 @@ import { SurfaceProvider, SocketIO } from '../../services/index';
 export class SurfaceComponent {
     constructor (private surfaceProvider: SurfaceProvider, private socketio: SocketIO) {
         socketio.connect();
+        this.disableBackNavigation();
     }
 
     @HostListener('window:resize', ['$event'])
     private onResize(event) {
         this.surfaceProvider.setSize(window.innerWidth, window.innerHeight);
+    }
+
+
+    /*
+     * Adapted from http://stackoverflow.com/a/12381873/4090817
+     */
+    private disableBackNavigation() {
+        window.location.href += "#";
+
+        setTimeout(function () {
+            window.location.href += "!";
+        }, 50); 
+    }
+
+    @HostListener('window:hashchange', ['$event'])
+    private onHashChange(event) {
+        if (window.location.hash !== "!") {
+            window.location.hash = "!";
+        }
     }
 }
