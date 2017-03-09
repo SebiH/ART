3a8b43a695db3935929e80b0b2b0507ffd4b9ae8
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2017-03-09 19:26:17 +0100

    Style improvements for dimension selector

--- interactivedisplay/client/app/components/graph-dimension-selector/graph-dimension-selector.component.ts
+++ interactivedisplay/client/app/components/graph-dimension-selector/graph-dimension-selector.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { GraphDataProvider } from '../../services/index';
 import { Graph, ChartDimension } from '../../models/index'; 
 
+const BORDER_SIZE = 30;
+
 @Component({
     selector: 'graph-dimension-selector',
     templateUrl: './app/components/graph-dimension-selector/graph-dimension-selector.html',
@@ -10,6 +12,7 @@ import { Graph, ChartDimension } from '../../models/index';
 export class GraphDimensionSelectorComponent implements OnInit {
     @Input() axis: 'x' | 'y';
     @Input() graph: Graph;
+    @Input() size: number = 1000;
 
     private dimensions: string[];
     private offset: number = 0;
@@ -18,12 +21,26 @@ export class GraphDimensionSelectorComponent implements OnInit {
         transform: ''
     };
 
+    private scrollerStyle: any = {};
+
     constructor(private graphDataProvider: GraphDataProvider) {}
 
     ngOnInit() {
         this.graphDataProvider.getDimensions()
             .first()
             .subscribe((dims) => this.dimensions = dims);
+
+        if (this.axis === 'x') {
+            this.scrollerStyle = {
+                'max-width': this.size - BORDER_SIZE * 2,
+                'margin': '0 ' + BORDER_SIZE + 'px'
+            };
+        } else {
+            this.scrollerStyle = {
+                'max-height': this.size - BORDER_SIZE * 2,
+                'margin': BORDER_SIZE + 'px 0'
+            };
+        }
     }
 
     private setDimension(dim: string): void {
@@ -54,7 +71,6 @@ export class GraphDimensionSelectorComponent implements OnInit {
     }
 
     private onDragUpdate(ev: any): void {
-        console.log(ev);
         if (this.axis === 'x') {
             this.offset -= ev.deltaX;
         } else {
