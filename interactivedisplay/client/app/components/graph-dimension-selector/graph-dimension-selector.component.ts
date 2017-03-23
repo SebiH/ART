ed1c1520a37da60c4bee697b2cf8ecfbc014cfcd
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2017-03-23 19:45:46 +0100

    Add interactions to triangles in dimension selector
    Closes #171

--- interactivedisplay/client/app/components/graph-dimension-selector/graph-dimension-selector.component.ts
+++ interactivedisplay/client/app/components/graph-dimension-selector/graph-dimension-selector.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, AfterViewInit, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 import { GraphDataProvider, FilterProvider } from '../../services/index';
 import { Graph, ChartDimension, Filter } from '../../models/index'; 
 
@@ -19,11 +20,20 @@ export class GraphDimensionSelectorComponent implements AfterViewInit {
     private dimensions: string[];
     private offset: number = 0;
     private maxOffset: number = 0;
+    private hasTouchDown: boolean = false;
 
     private scrollerStyle: any = {};
     private listStyle = {
         '-webkit-transform': '',
-        transform: ''
+        transform: '',
+        transition: ''
+    };
+
+    private triangleBeforeStyle = {
+        'opacity': 1,
+    };
+    private triangleAfterStyle = {
+        'opacity': 1
     };
 
     constructor(private graphDataProvider: GraphDataProvider, private filterProvider: FilterProvider) {}
@@ -43,7 +53,7 @@ export class GraphDimensionSelectorComponent implements AfterViewInit {
                 let graphDim = (this.axis == 'x') ? this.graph.dimX : this.graph.dimY;
                 let itemSize = (this.axis == 'x') ? ITEM_WIDTH : ITEM_HEIGHT;
                 if (graphDim) {
-                    this.offset = -(dims.indexOf(graphDim) * itemSize - this.size / 2 + BORDER_SIZE * 2);
+                    this.offset = dims.indexOf(graphDim) * itemSize - this.size / 2 + BORDER_SIZE * 2;
                     this.updateOffset();
                 }
             });
@@ -76,39 +86,120 @@ export class GraphDimensionSelectorComponent implements AfterViewInit {
         }
     }
 
+    private getItemSize(): number {
+        return (this.axis == 'x') ? ITEM_WIDTH : ITEM_HEIGHT;
+    }
+
     private updateOffset(): void {
-        this.offset = -Math.max(-this.size / 2, Math.min(-this.offset, this.maxOffset));
+        this.offset = Math.max(-this.size / 2, Math.min(this.offset, this.maxOffset));
 
         // calculate max offset
         let transform = '';
         if (this.axis === 'x') {
-            transform = 'translate3d(' + this.offset + 'px, 0, 0)';
+            transform = 'translate3d(' + (-this.offset) + 'px, 0, 0)';
         } else {
-            transform = 'translate3d(0, ' + this.offset + 'px, 0)';
+            transform = 'translate3d(0, ' + (-this.offset) + 'px, 0)';
+        }
+
+        let transition = '';
+        if (!this.hasTouchDown) {
+            transition = 'transform 0.5s ease-out';
         }
 
         this.listStyle['-webkit-transform'] = transform;
         this.listStyle.transform = transform;
+        this.listStyle.transition = transition;
+
+
+        let itemSize = this.getItemSize();
+        if (this.offset <= itemSize / 2) {
+            this.triangleBeforeStyle.opacity = 0;
+        } else {
+            this.triangleBeforeStyle.opacity = 1;
+        }
+
+        if (this.offset >= this.maxOffset - this.size / 2 - itemSize / 2) {
+            this.triangleAfterStyle.opacity = 0;
+        } else {
+            this.triangleAfterStyle.opacity = 1;
+        }
     }
 
 
 
     private onDragBegin(ev: any): void {
-
+        this.hasTouchDown = true;
+        this.updateOffset();
     }
 
     private onDragUpdate(ev: any): void {
         if (this.axis === 'x') {
-            this.offset -= ev.deltaX;
+            this.offset += ev.deltaX;
         } else {
-            this.offset -= ev.deltaY;
+            this.offset += ev.deltaY;
         }
 
         this.updateOffset();
     }
 
     private onDragEnd(ev: any): void {
-
+        this.hasTouchDown = false;
+        this.updateOffset();
     }
+
+
+
+    private scrollBack(): void {
+        let itemSize = this.getItemSize();
+        this.offset -= itemSize;
+        this.updateOffset();
+    }
+
+    private scrollForward(): void {
+        let itemSize = this.getItemSize();
+        this.offset += itemSize;
+        this.updateOffset();
+    }
+
+
+
+    private hasBeforeTouchDown: boolean = false;
+
+    private onBeforeTouchDown(): void {
+        let itemSize = this.getItemSize();
+        this.hasBeforeTouchDown = true;
+        Observable.timer(0, 100)
+            .takeWhile(() => this.hasBeforeTouchDown && this.offset >= 0)
+            .subscribe(() => {
+                this.offset -= itemSize / 3;
+                this.updateOffset();
+            });
+    }
+
+    private onBeforeTouchUp(): void {
+        this.hasBeforeTouchDown = false;
+    }
+
+
+
+
+
+    private hasAfterTouchDown: boolean = false;
+
+    private onAfterTouchDown(): void {
+        let itemSize = this.getItemSize();
+        this.hasAfterTouchDown = true;
+        Observable.timer(0, 100)
+            .takeWhile(() => this.hasAfterTouchDown && this.offset < this.maxOffset)
+            .subscribe(() => {
+                this.offset += itemSize / 3;
+                this.updateOffset();
+            });
+    }
+
+    private onAfterTouchUp(): void {
+        this.hasAfterTouchDown = false;
+    }
+
 }
 
