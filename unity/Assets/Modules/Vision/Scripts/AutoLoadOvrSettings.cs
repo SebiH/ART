406e5a6b245e356ceb20661204efaa4e48b8c6bc
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2017-04-17 00:52:30 +0200

    Automatically load camera parameters at startup
    Towards #61

---
+++ unity/Assets/Modules/Vision/Scripts/AutoLoadOvrSettings.cs
@@ -0,0 +1,45 @@
+using Assets.Modules.Core;
+using Assets.Modules.Vision.CameraSources;
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Modules.Vision
+{
+    [RequireComponent(typeof(OvrvisionCameraSource))]
+    public class AutoLoadOvrSettings : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            StartCoroutine(LoadStartup());
+        }
+
+        private IEnumerator LoadStartup()
+        {
+            // wait until camera is initialized
+            yield return new WaitForSeconds(5f);
+            LoadSettings();
+        }
+
+        public void LoadSettings()
+        {
+            try
+            {
+                var settingsJson = FileUtility.LoadFromFile(Globals.OvrSettingsSavefile);
+                var settings = JsonUtility.FromJson<OvrSettings>(settingsJson);
+
+                var ovrCamera = GetComponent<OvrvisionCameraSource>();
+                ovrCamera.Gain = settings.Gain;
+                ovrCamera.Exposure = settings.Exposure;
+                ovrCamera.BLC = settings.BLC;
+
+                Debug.Log("Successfully loaded ovr camera parameters");
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Could not autoload ovr camera parameters:");
+                Debug.LogError(e.Message);
+            }
+        }
+    }
+}
