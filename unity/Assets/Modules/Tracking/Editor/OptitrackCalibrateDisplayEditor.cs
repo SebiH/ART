e73ffeca066f6f8f5934aad447c61c296aa1af39
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2017-04-17 00:43:19 +0200

    Remove duplicate namespace

--- unity/Assets/Modules/Tracking/Editor/OptitrackCalibrateDisplayEditor.cs
+++ unity/Assets/Modules/Tracking/Editor/OptitrackCalibrateDisplayEditor.cs
@@ -3,38 +3,34 @@ using UnityEngine;
 
 namespace Assets.Modules.Tracking
 {
-    namespace Assets.Modules.Calibration
+    [CustomEditor(typeof(OptitrackCalibrateDisplay))]
+    public class OptitrackCalibrateDisplayEditor : Editor
     {
-        [CustomEditor(typeof(OptitrackCalibrateDisplay))]
-        public class OptitrackCalibrateDisplayEditor : Editor
+        public override void OnInspectorGUI()
         {
-            public override void OnInspectorGUI()
+            base.OnInspectorGUI();
+            var script = target as OptitrackCalibrateDisplay;
+
+            if (!script.IsCalibrating)
             {
-                base.OnInspectorGUI();
-                var script = target as OptitrackCalibrateDisplay;
-
-                if (!script.IsCalibrating)
+                if (GUILayout.Button("Set Corner") && Application.isPlaying)
                 {
-                    if (GUILayout.Button("Set Corner") && Application.isPlaying)
-                    {
-                        script.StartCalibration();
-                    }
+                    script.StartCalibration();
+                }
 
-                    if (GUILayout.Button("Commit display") && Application.isPlaying)
-                    {
-                        script.CommitFixedDisplay();
-                    }
-                }
-                else
+                if (GUILayout.Button("Commit display") && Application.isPlaying)
                 {
-                    Rect r = EditorGUILayout.BeginVertical();
-                    EditorGUI.ProgressBar(r, script.CalibrationProgress, "Calibrating...");
-                    GUILayout.Space(19);
-                    EditorGUILayout.EndVertical();
-                    Repaint();
+                    script.CommitFixedDisplay();
                 }
+            }
+            else
+            {
+                Rect r = EditorGUILayout.BeginVertical();
+                EditorGUI.ProgressBar(r, script.CalibrationProgress, "Calibrating...");
+                GUILayout.Space(19);
+                EditorGUILayout.EndVertical();
+                Repaint();
             }
         }
     }
-
 }
