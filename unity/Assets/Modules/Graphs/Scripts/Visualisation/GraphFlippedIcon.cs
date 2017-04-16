d2bade2e3e3e9192211ced7d8bfee3da05553a2c
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2017-04-16 15:42:01 +0200

    Add icon if graph is flipped

---
+++ unity/Assets/Modules/Graphs/Scripts/Visualisation/GraphFlippedIcon.cs
@@ -0,0 +1,24 @@
+using Assets.Modules.Core;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Modules.Graphs.Visualisation
+{
+    [RequireComponent(typeof(Image))]
+    public class GraphFlippedIcon : MonoBehaviour
+    {
+        private Graph _graph;
+        private Image _image;
+
+        private void OnEnable()
+        {
+            _graph = UnityUtility.FindParent<Graph>(this);
+            _image = GetComponent<Image>();
+        }
+
+        private void Update()
+        {
+            _image.enabled = _graph.IsFlipped;
+        }
+    }
+}
