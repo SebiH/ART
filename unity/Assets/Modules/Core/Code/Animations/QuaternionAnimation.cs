be60e923cbc905af0c69ce85a3cef137d0584805
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2017-04-23 00:01:13 +0200

    Prevent invalid quaternions in animation

--- unity/Assets/Modules/Core/Code/Animations/QuaternionAnimation.cs
+++ unity/Assets/Modules/Core/Code/Animations/QuaternionAnimation.cs
@@ -10,6 +10,11 @@ namespace Assets.Modules.Core.Animations
 
         protected override Quaternion Lerp(Quaternion start, Quaternion end, float weight)
         {
+            if (Mathf.Abs(start.x) < Mathf.Epsilon && Mathf.Abs(start.y) < Mathf.Epsilon && Mathf.Abs(start.z) < Mathf.Epsilon && Mathf.Abs(start.w) < Mathf.Epsilon)
+                return Quaternion.identity;
+            if (Mathf.Abs(end.x) < Mathf.Epsilon && Mathf.Abs(end.y) < Mathf.Epsilon && Mathf.Abs(end.z) < Mathf.Epsilon && Mathf.Abs(end.w) < Mathf.Epsilon)
+                return Quaternion.identity;
+
             return Quaternion.Lerp(start, end, weight);
         }
     }
