715f487829dbeaf07185e05e06aeafbcf4a224a1
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2017-04-23 17:06:35 +0200

    Add editor to set ar marker size manually

---
+++ unity/Assets/Modules/Tracking/Editor/ArMarkerTrackerEditor.cs
@@ -0,0 +1,35 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Modules.Tracking
+{
+    //[CustomEditor(typeof(ArToolkitListener))]
+    [CustomEditor(typeof(ArToolkitStereoListener))]
+    //[CustomEditor(typeof(ArucoMarkerTracker))]
+    public class ArMarkerTrackerEditor : Editor
+    {
+        private string _arMarkerSize = "10";
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            var script = target as ArMarkerTracker;
+
+            GUILayout.Label("ArMarkerSize (in m?)");
+            _arMarkerSize = GUILayout.TextField(_arMarkerSize);
+
+            if (GUILayout.Button("Set Marker Size") && Application.isPlaying)
+            {
+                try
+                {
+                    var size = float.Parse(_arMarkerSize);
+                    script.MarkerSizeInMeter = Mathf.Max(size, 0.0001f);
+                }
+                catch
+                {
+                    Debug.LogError("Unable to parse " + _arMarkerSize);
+                }
+            }
+        }
+    }
+}
