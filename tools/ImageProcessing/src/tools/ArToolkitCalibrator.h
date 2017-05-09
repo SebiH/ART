bedc43c270d5fe59e1a2e8d0a6408cbe5f7d014e
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2017-05-09 23:42:47 +0200

    Use precalculated intrinsic matrix without distortion coefficitents for artk calibration

--- tools/ImageProcessing/src/tools/ArToolkitCalibrator.h
+++ tools/ImageProcessing/src/tools/ArToolkitCalibrator.h
@@ -26,7 +26,7 @@ namespace ImageProcessing
 
 	private:
 		double computeReprojectionErrors(const std::vector<std::vector<cv::Point3f>> &object_points, const std::vector<std::vector<cv::Point2f>> &image_points, const std::vector<cv::Mat> &rvecs, const std::vector<cv::Mat> &tvecs, const cv::Mat &camera_matrix, const cv::Mat &dist_coeffs, std::vector<float> &per_view_errors);
-		double SingleCameraCalibration(const std::string &filename, const std::vector<std::vector<cv::Point2f>> &image_points, const cv::Size &image_size);
+		double SingleCameraCalibration(const std::shared_ptr<CameraSourceInterface> &camera, const std::string &filename, const std::vector<std::vector<cv::Point2f>> &image_points, const cv::Size &image_size);
 		void ConvParam(float intr[3][4], float dist[4], int xsize, int ysize, ARParam *param);
 		void SaveParam(ARParam *param, const std::string &filename);
 		ARdouble GetSizeFactor(ARdouble dist_factor[], int xsize, int ysize, int dist_function_version);
