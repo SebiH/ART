c133c1e6497de4b43e4aa0fbbd196800adc96f0c
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2017-05-13 18:07:07 +0200

    Refactor

---
+++ interactivedisplay/server/src/colors.ts
@@ -0,0 +1,12 @@
+export const Red = '#E53935';
+export const Green = '#43A047';
+export const LightGreen = '#8BC34A';
+export const DeepGreen = '#2E7D32';
+export const Blue = '#1E88E5';
+export const Yellow = '#FDD835';
+export const Purple = '#9C27B0';
+export const Orange = '#F4511E';
+export const Cyan = '#00BCD4';
+export const White = '#FFFFFF';
+export const Black = '#000000';
+
