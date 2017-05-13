9aecd400663916befb05010b0b120bec52264dbf
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2017-05-13 22:24:36 +0200

    Implement marker overlay
    Closes #248

--- interactivedisplay/client/app/components/marker-overlay/marker-overlay.component.ts
+++ interactivedisplay/client/app/components/marker-overlay/marker-overlay.component.ts
@@ -1,18 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MarkerProvider } from '../../services/index';
+import { Marker } from '../../models/index';
 
 @Component({
     selector: 'marker-overlay',
-    template: `<div>X</div>`,
+    template: `
+<div class="overlay">
+    <div class="marker-row" *ngFor="let row of rows">
+        <div class="marker" *ngFor="let col of columns">
+            <ar-marker [marker]="markers[row * maxColumns + col]"></ar-marker>
+        </div>
+    </div>
+</div>`,
     styles: [
+        `.overlay {
+            width: 100%; height: 100%;
+            background: white;
+            display: flex;
+            align-items: center;
+            justify-content: flex-end;
+            flex-direction: column;
+        }`,
+        '.marker { margin: 30px; display: inline-block; }'
     ]
 })
+export class MarkerOverlayComponent implements OnInit, OnDestroy {
 
-export class MarkerOverlayComponent implements OnInit {
+    private maxRows: number = 4;
+    private rows: number[] = [];
+
+    private maxColumns: number = 4;
+    private columns: number[] = [];
+
+    private markers: Marker[] = [];
 
     constructor (private markerProvider: MarkerProvider) {
+        for (let i = 0; i < this.maxRows; i++) {
+            this.rows.push(i);
+        }
+        for (let i = 0; i < this.maxColumns; i++) {
+            this.columns.push(i);
+        }
     }
 
     ngOnInit() {
+        for (let i = 0; i < this.maxRows * this.maxColumns; i++) {
+            this.markers.push(this.markerProvider.createMarker());
+        }
+    }
+
+    ngOnDestroy() {
+        for (let marker of this.markers) {
+            this.markerProvider.destroyMarker(marker);
+        }
     }
 }
