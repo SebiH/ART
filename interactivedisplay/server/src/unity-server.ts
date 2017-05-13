73280abe9d1ab234d52a382f722ab7bc086707bb
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2017-05-13 22:28:42 +0200

    Disable nagle's algorithm for server - unity connection
    Trying to improve #231

--- interactivedisplay/server/src/unity-server.ts
+++ interactivedisplay/server/src/unity-server.ts
@@ -42,12 +42,12 @@ export class UnityServer {
             let charcode = str.charCodeAt(i);
             if (charcode < 0x80) { utf8.push(charcode); }
             else if (charcode < 0x800) {
-                utf8.push(0xc0 | (charcode >> 6), 
+                utf8.push(0xc0 | (charcode >> 6),
                     0x80 | (charcode & 0x3f));
             }
             else if (charcode < 0xd800 || charcode >= 0xe000) {
-                utf8.push(0xe0 | (charcode >> 12), 
-                    0x80 | ((charcode>>6) & 0x3f), 
+                utf8.push(0xe0 | (charcode >> 12),
+                    0x80 | ((charcode>>6) & 0x3f),
                     0x80 | (charcode & 0x3f));
             }
             // surrogate pair
@@ -58,9 +58,9 @@ export class UnityServer {
                 // 20 bits of 0x0-0xFFFFF into two halves
                 charcode = 0x10000 + (((charcode & 0x3ff)<<10)
                     | (str.charCodeAt(i) & 0x3ff));
-                utf8.push(0xf0 | (charcode >>18), 
-                    0x80 | ((charcode>>12) & 0x3f), 
-                    0x80 | ((charcode>>6) & 0x3f), 
+                utf8.push(0xf0 | (charcode >>18),
+                    0x80 | ((charcode>>12) & 0x3f),
+                    0x80 | ((charcode>>6) & 0x3f),
                     0x80 | (charcode & 0x3f));
             }
         }
@@ -82,6 +82,8 @@ export class UnityServer {
     private handleConnection(socket: net.Socket): void {
         let address = socket.address().address;
         console.log(LOG_PREFIX + "New unity client connected from " + address);
+
+        socket.setNoDelay(true);
 
         this.handleNewConnection(socket);
 
