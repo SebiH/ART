908f8c78d2b0d1427d6eb2db50f7a6fbce12af42
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2017-06-21 13:26:41 +0200

    Allow database switching

--- interactivedisplay/server/quickstart.sh
+++ interactivedisplay/server/quickstart.sh
@@ -5,10 +5,17 @@ cp "sql.conf-$1.json" "sql.conf.json"
 echo "Initialized configuration"
 
 if [[ $2 ]]; then
-    echo "Loading new saved state"
+    echo "Loading new saved state $2"
     cp "data/cache-$2.json" "data/cache.json"
 else
     echo "Keeping current state"
+fi
+
+if [[ $3 ]]; then
+    echo "Switching to database $3"
+    sed -i "s/\\\"database\\\": \\\"[^\\\"]*\\\"/\\\"database\\\": \\\"$3\\\"/g" sql.conf.json
+else
+    echo "Keeping current database"
 fi
 
 echo "Starting server"
