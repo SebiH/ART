cc7873367340a7d1376fe2bd294be9878867e0cc
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2017-06-21 14:24:15 +0200

    Save snapshot for prev config instead of overwriting

--- interactivedisplay/server/quickstart.sh
+++ interactivedisplay/server/quickstart.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 echo "Switching to $1"
 
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+
+cp "sql.conf.json" "sql.conf-$TIMESTAMP.json"
 cp "sql.conf-$1.json" "sql.conf.json"
 echo "Initialized configuration"
 
 if [[ $2 ]]; then
     echo "Loading new saved state $2"
+    cp "data/cache.json" "data/$TIMESTAMP-cache.json"
     cp "data/cache-$2.json" "data/cache.json"
 else
     echo "Keeping current state"
