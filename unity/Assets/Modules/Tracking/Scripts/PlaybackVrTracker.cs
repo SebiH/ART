c267f4026b6f20d3ac863091dd12fe7b8b381554
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2017-08-12 22:49:53 +0200

    Add smoothing to vr playback

--- unity/Assets/Modules/Tracking/Scripts/PlaybackVrTracker.cs
+++ unity/Assets/Modules/Tracking/Scripts/PlaybackVrTracker.cs
@@ -1,5 +1,6 @@
 using Assets.Modules.Core;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
@@ -10,6 +11,7 @@ namespace Assets.Modules.Tracking
         public string Filename;
         [Range(-1, 1)]
         public float TimeOffset = 0;
+        public int SmoothAverage = 3;
 
         public enum PlaybackTrackedDevice
         {
@@ -40,25 +42,60 @@ namespace Assets.Modules.Tracking
 
             if (_currentIndex < _cache.entries.Length)
             {
-                switch (TrackedDevice)
+                VRListener.PoseUpdateTime = _cache.entries[_currentIndex].Time;
+
+                List<Vector3> positions = new List<Vector3>();
+                positions.Add(GetPosition(_currentIndex));
+                for (var i = 1; i <= SmoothAverage; i++)
                 {
-                    case PlaybackTrackedDevice.Head:
-                        VRListener.CurrentPosition = _cache.entries[_currentIndex].HeadPos;
-                        VRListener.CurrentRotation = _cache.entries[_currentIndex].HeadRot;
-                        VRListener.PoseUpdateTime = _cache.entries[_currentIndex].Time;
-                        break;
-                    case PlaybackTrackedDevice.LeftHand:
-                        VRListener.CurrentPosition = _cache.entries[_currentIndex].LHPos;
-                        VRListener.CurrentRotation = _cache.entries[_currentIndex].LHRot;
-                        VRListener.PoseUpdateTime = _cache.entries[_currentIndex].Time;
-                        break;
-                    case PlaybackTrackedDevice.RightHand:
-                        VRListener.CurrentPosition = _cache.entries[_currentIndex].RHPos;
-                        VRListener.CurrentRotation = _cache.entries[_currentIndex].RHRot;
-                        VRListener.PoseUpdateTime = _cache.entries[_currentIndex].Time;
-                        break;
+                    if (_currentIndex + i < _cache.entries.Length)
+                        positions.Add(GetPosition(_currentIndex + i));
+
+                    if (_currentIndex - i >= 0)
+                        positions.Add(GetPosition(_currentIndex - i));
                 }
+                VRListener.CurrentPosition = MathUtility.Average(positions);
+
+                List<Quaternion> rotations = new List<Quaternion>();
+                rotations.Add(GetRotation(_currentIndex));
+                for (var i = 1; i <= SmoothAverage; i++)
+                {
+                    if (_currentIndex + i < _cache.entries.Length)
+                        rotations.Add(GetRotation(_currentIndex + i));
+
+                    if (_currentIndex - i >= 0)
+                        rotations.Add(GetRotation(_currentIndex - i));
+                }
+                VRListener.CurrentRotation = MathUtility.Average(rotations);
             }
+        }
+
+        private Vector3 GetPosition(int index)
+        {
+            switch (TrackedDevice)
+            {
+                case PlaybackTrackedDevice.Head:
+                    return _cache.entries[index].HeadPos;
+                case PlaybackTrackedDevice.LeftHand:
+                    return _cache.entries[index].LHPos;
+                case PlaybackTrackedDevice.RightHand:
+                    return _cache.entries[index].RHPos;
+            }
+            return Vector3.zero;
+        }
+
+        private Quaternion GetRotation(int index)
+        {
+            switch (TrackedDevice)
+            {
+                case PlaybackTrackedDevice.Head:
+                    return _cache.entries[index].HeadRot;
+                case PlaybackTrackedDevice.LeftHand:
+                    return _cache.entries[index].LHRot;
+                case PlaybackTrackedDevice.RightHand:
+                    return _cache.entries[index].RHRot;
+            }
+            return Quaternion.identity;
         }
 
         private void OnDisable()
