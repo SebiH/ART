5f9de19084a49863bdaed9c9a3da09c8d6fc626a
test@test <test@test>
2017-08-18 13:55:30 +0200

    Screenshot test script

---
+++ unity/Assets/Modules/Vision/Scripts/ScreenshotVideo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Modules.Vision
+{
+    public class ScreenshotVideo : MonoBehaviour
+    {
+        public string Prefix = "cam_";
+        private int counter = 0;
+
+        private void Update()
+        {
+            Application.CaptureScreenshot("D:/GeforceShare/test/" + Prefix + counter + ".jpg", 8);
+            counter++;
+        }
+    }
+}
