195e06af470bc59371c8fb96a5454571b6c1d79b
Sebastian Hubenschmid <sebastianhubenschmid@gmail.com>
2017-09-12 18:46:43 +0200

    Add timeout for creating new graphs

--- interactivedisplay/client/app/components/graph-create-button/graph-create-button.component.ts
+++ interactivedisplay/client/app/components/graph-create-button/graph-create-button.component.ts
@@ -13,26 +13,43 @@ export class GraphCreateButtonComponent {
     private graphCreation = new EventEmitter();
 
     private createdGraph: Graph;
+    private lastGraphCreation: number = 0;
 
     constructor(private graphProvider: GraphProvider) {}
 
     private handleCreateStart(event: any): void {
-        this.graphProvider.selectGraph(null);
-        let graph = this.graphProvider.addGraph();
-        this.createdGraph = graph;
-        this.createdGraph.isPickedUp = true;
-        this.graphCreation.emit(this.createdGraph);
+        this.graphProvider.getGraphs()
+            .first()
+            .subscribe((graphs) => {
+
+                let currentTime = new Date().getTime();
+
+                // only allow one graph created at a time, with timeout
+                if (!graphs.find((g) => g.isNewlyCreated) &&  currentTime - this.lastGraphCreation > 1500) {
+                    this.lastGraphCreation = currentTime;
+                    this.graphProvider.selectGraph(null);
+                    let graph = this.graphProvider.addGraph();
+                    this.createdGraph = graph;
+                    this.createdGraph.isPickedUp = true;
+                    this.graphCreation.emit(this.createdGraph);
+                }
+            });
+
     }
 
     private handleCreateUpdate(event: any): void {
-        this.createdGraph.posOffset -= event.deltaX;
+        if (this.createdGraph) {
+            this.createdGraph.posOffset -= event.deltaX;
+        }
     }
 
     private handleCreateEnd(event: any): void {
-        this.createdGraph.isPickedUp = false;
-        this.createdGraph.isNewlyCreated = false;
-        this.createdGraph.posOffset = 0;
-        this.graphProvider.selectGraph(this.createdGraph);
-        this.createdGraph = null;
+        if (this.createdGraph) {
+            this.createdGraph.isPickedUp = false;
+            this.createdGraph.isNewlyCreated = false;
+            this.createdGraph.posOffset = 0;
+            this.graphProvider.selectGraph(this.createdGraph);
+            this.createdGraph = null;
+        }
     }
 }
